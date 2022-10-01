387f00a149ec4cff10a0e48bab44bb40f4f07566
raygdev <101662915+raygdev@users.noreply.github.com>
2022-10-01 17:23:28 -0400

    Add path module and use path

--- server/server.js
+++ server/server.js
@@ -1,15 +1,18 @@
 const express = require('express')
 const helmet = require('helmet')
 const cors = require('cors')
+const path = require('path')
 require('dotenv').config()
 
 const port = process.env.MY_PORT
 const app = express()
+app.use(express.static(path.join(__dirname, 'build',)))
 
 app.use(cors())
 app.use(helmet())
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
+
 
 app.get('/api', (req,res,next) => {
     console.log('hello from express')
