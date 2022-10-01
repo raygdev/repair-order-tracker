cfb5355e793d83a97b74fb0d367917252395901f
raygdev <101662915+raygdev@users.noreply.github.com>
2022-10-01 19:13:46 -0400

    Add repair order schema for mongodb

--- server/models/repairOrderModel.js
+++ server/models/repairOrderModel.js
@@ -1,3 +1,22 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-module.exports = RepairOrder
+const RepairOrderSchema = new Schema({
+  ro_number: { type: Number, require: true },
+  isWarranty: { type: Boolean, required: true },
+  tech_id: { type: String, required: true },
+  vin: {
+    type: String,
+    lowercase: true,
+    trim: true,
+    validate: {
+      validator: function (v) {
+        return /\w{17}/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid vin!`,
+    },
+    required: [true, "vin is required, must be 17 characters"],
+  },
+  created_on: { type: Date, default: new Date() },
+});
+
+module.exports = RepairOrderSchema;
