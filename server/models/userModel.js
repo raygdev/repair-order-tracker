3283acceb0014c52152ae063590b4bd839669c5e
raygdev <101662915+raygdev@users.noreply.github.com>
2022-10-01 19:15:19 -0400

    Add user model for mongodb

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,2 +1,19 @@
-const mongoose = require('mongoose')
-const RepairOrder = require('./repairOrderModel')
+const mongoose = require("mongoose");
+const RepairOrderSchema = require("./repairOrderModel");
+
+const UserSchema = new Schema({
+  name: {
+    first: String,
+    last: String,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password:{
+    type: String,
+    required: true,
+  },
+  repair_orders:[RepairOrderSchema]
+});
