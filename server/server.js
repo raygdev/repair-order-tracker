1a28b4b039d1b364f04dbbcbc1810075879acd3a
raygdev <101662915+raygdev@users.noreply.github.com>
2022-10-02 13:34:56 -0400

    Provide connected mongoose client
    connects a database to the server using
    mongoose.connect and passing uri

--- server/server.js
+++ server/server.js
@@ -2,9 +2,16 @@ const express = require('express')
 const helmet = require('helmet')
 const cors = require('cors')
 const path = require('path')
+const mongoose = require('mongoose')
 require('dotenv').config()
 
-const port = process.env.MY_PORT
+const port = process.env.PORT
+const uri = process.env.MONGO_URI
+
+const client = mongoose.connect(uri,{},(err) => {
+    if(err) return console.error(err.reason)
+})
+
 const app = express()
 app.use(express.static(path.join(__dirname, 'build',)))
 
