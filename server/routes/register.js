7f4f3db2c064ac2d310369f6a598e8753e3641b5
raygdev <101662915+raygdev@users.noreply.github.com>
2022-10-02 13:49:48 -0400

    Change to a specific route
    change from dynamic route to specific route to
    register new users.

--- server/routes/register.js
+++ server/routes/register.js
@@ -5,7 +5,7 @@ const User = require("./models/userSchema.js");
 const UserSchema = User.UserSchema;
 const createAndUpdateUser = User.createAndUpdateUser;
 
-router.get("/api/register/:user", (req, res, next) => {
+router.get("/api/register/user", (req, res, next) => {
   const user = req.body;
   createAndUpdateUser(user, (err, newUser) => {
     if (err) return res.send(err.error);
