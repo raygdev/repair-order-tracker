f90e0651601ee39871eb11091b85d35a25a39eb9
raygdev <101662915+raygdev@users.noreply.github.com>
2022-10-02 19:10:28 -0400

    Add registration form for new user
    add registration form and inputs for new
    user
    
    provide use effect to run fetch call to
    server with the required form inputs
    
    call creates new user or sends error
    to client for duplicate emails.

---
+++ client/src/components/Register.jsx
@@ -0,0 +1,141 @@
+import React, { useState, useEffect } from "react";
+import "../css/registerForm.css";
+
+export const Register = (props) => {
+  const [newUser, setNewUser] = useState({
+    name: {
+      first: "",
+      last: "",
+    },
+    shop_name: "",
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState("");
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (isLoading) {
+      fetch("/api/register/user", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newUser),
+      })
+        .then((res) => {
+          if (!res.ok) {
+            throw res.json();
+          } else {
+            return res.json();
+          }
+        })
+        .then((data) => {
+          console.log(data.message);
+          console.log(data.you)
+          console.log("came here second then block");
+          setIsLoading(false);
+          setIsSubmitted(true);
+        })
+        .catch((e) => {
+          e.then((result) => {
+            console.log(result.error);
+            setError(result.error);
+            setIsLoading(false);
+          });
+          console.log("came here for error handling catch");
+        });
+    }
+  }, [isLoading]);
+
+  function handleChange(e) {
+    let { name, value } = e.target;
+    setNewUser((prevUser) => {
+      if (name === "name.first") {
+        return {
+          ...prevUser,
+          name: {
+            first: value,
+            last: prevUser.name.last,
+          },
+        };
+      } else if (name === "name.last") {
+        return {
+          ...prevUser,
+          name: {
+            first: prevUser.name.first,
+            last: value,
+          },
+        };
+      } else {
+        return {
+          ...prevUser,
+          [name]: value,
+        };
+      }
+    });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setIsLoading(true);
+  }
+
+  const style = {
+    display: error ? "block" : "none",
+    color: "red",
+    fontWeight: "bold",
+  };
+
+  const buttonStyle = {
+    backgroundColor: isSubmitted ? "green" : "yellow",
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name={"name.first"}
+        value={newUser.name.first}
+        required={true}
+        onChange={handleChange}
+        placeholder="First Name"
+      />
+      <input
+        type="text"
+        name={"name.last"}
+        value={newUser.name.last}
+        required={true}
+        onChange={handleChange}
+        placeholder="Last Name"
+      />
+      <input
+        type="text"
+        name={"shop_name"}
+        value={newUser.shop_name}
+        onChange={handleChange}
+        placeholder="Shop Name"
+      />
+      <input
+        type="email"
+        name={"email"}
+        value={newUser.email}
+        required={true}
+        onChange={handleChange}
+        placeholder="Enter your email"
+      />
+      <input
+        type="password"
+        name={"password"}
+        value={newUser.password}
+        onChange={handleChange}
+        placeholder="Create a Password"
+      />
+      <button style={buttonStyle} disabled={isSubmitted || isLoading}>
+        {isSubmitted ? "Success" : "Submit"}
+      </button>
+      <p style={style}>{error}</p>
+    </form>
+  );
+};
