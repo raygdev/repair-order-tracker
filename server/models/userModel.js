28da969c45d270632beb65cc9024b5f872f4c046
raygdev <101662915+raygdev@users.noreply.github.com>
2022-10-02 10:48:03 -0400

    Add email validation to user model
    adds email validation to user model and exports
    model from file

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 const RepairOrderSchema = require("./repairOrderModel");
 
+const emailValidate =
+  /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+
 const UserSchema = new Schema({
   name: {
     first: String,
@@ -8,12 +11,23 @@ const UserSchema = new Schema({
   },
   email: {
     type: String,
-    required: true,
-    unique: true
+    unique: true,
+    validate: {
+      validator: function (v) {
+        return emailValidate.test(v);
+      },
+      message: function (props) {
+        return "Email is invalid";
+      },
+    },
+    required: [true, "Must be a valid email"],
   },
-  password:{
+  password: {
     type: String,
     required: true,
   },
-  repair_orders:[RepairOrderSchema]
+  shop_name: { type: String },
+  repair_orders: [RepairOrderSchema],
 });
+
+module.exports = UserSchema;
