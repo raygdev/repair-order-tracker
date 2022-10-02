b95c8d2393b9d4286d0a67d0e12818e95a1c4a7b
raygdev <101662915+raygdev@users.noreply.github.com>
2022-10-02 13:38:09 -0400

    Add route for registering user

---
+++ server/routes/register.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+
+const User = require("./models/userSchema.js");
+const UserSchema = User.UserSchema;
+const createAndUpdateUser = User.createAndUpdateUser;
+
+router.get("/api/register/:user", (req, res, next) => {
+  const user = req.body;
+  createAndUpdateUser(user, (err, newUser) => {
+    if (err) return res.send(err.error);
+    res.send(newUser);
+  });
+});
+
+
+module.exports = router
