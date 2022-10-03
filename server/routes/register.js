cef231370ef777057b9173cafb22955f4fb6542d
raygdev <101662915+raygdev@users.noreply.github.com>
2022-10-02 21:27:35 -0400

    Remove logic for error handling from route
    utilize error handling function from utils folder

--- server/routes/register.js
+++ server/routes/register.js
@@ -1,26 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { MongoServerError } = require('mongoose')
 
-const {createAndSaveUser} = require("../models/userModel.js")
-
+const { createAndSaveUser } = require("../models/userModel.js");
+const { handleRegistrationError } = require("../uitls/utils.js");
 
 router.post("/api/register/user", (req, res, next) => {
- return  createAndSaveUser(req.body, (err, newUser) => {
-    if (err) {
-        console.log(err.message)
-        console.log(err.message.includes('duplicate key'))
-            if(err.message.includes('duplicate key')){
-               return res.status(401).send({error: 'User Already Exists'})
-            } else {
-                return res.status(404).send({error: 'Unknow Error'})
-            }
-    } else {
-        console.log('user block')
-       return res.status(200).send({message: 'ok', you: newUser});
-    }
+  return createAndSaveUser(req.body, (err, newUser) => {
+      if(err) return handleRegistrationError(res, err)
+      return res.status(200).send({ message: "ok", you: newUser });
   });
 });
 
-
-module.exports = router
+module.exports = router;
