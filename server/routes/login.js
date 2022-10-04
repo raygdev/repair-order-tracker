ad72879b904bdbe3c34a11f2f9ae6e5e1ae5d0eb
raygdev <101662915+raygdev@users.noreply.github.com>
2022-10-04 18:37:31 -0400

    Utilize find user in login route

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const router = express.Router()
 
+const userModel = require('../models/userModel')
+
 router.post('/api/login',(req,res,next) => {
-    console.log('in the login route')
-    next()
+    userModel.findUserByEmailAndPassword(req.body,(err,user) => {
+        if(err){
+            console.log(err)
+            return
+        }else if(!user){
+            console.log(`Can't seem to find the user`)
+            return
+        } else {
+            console.log(user)
+            return
+        }
+    })
 })
 
 module.exports = router
