3570f9adb9d0b54937e3977c49e99364bfce12e4
raygdev <101662915+raygdev@users.noreply.github.com>
2022-10-04 20:09:10 -0400

    Use route to send error or user response

--- server/routes/login.js
+++ server/routes/login.js
@@ -6,13 +6,16 @@ const userModel = require('../models/userModel')
 router.post('/api/login',(req,res,next) => {
     userModel.findUserByEmailAndPassword(req.body,(err,user) => {
         if(err){
-            console.log(err)
+            res.send(err)
             return
         }else if(!user){
-            console.log(`Can't seem to find the user`)
+            res.status(404).send(`can't seem to find that user`)
+            return
+        } else if((req.body.email !== user.email) && (req.body.passord !== user.password)) {
+            res.status(404).send(`username/password combination doesn't exist`)
             return
         } else {
-            console.log(user)
+            res.status(200).send(user)
             return
         }
     })
