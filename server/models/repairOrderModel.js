87ab2262d57eb9ff5a68d12dfa09da0e21935383
raygdev <101662915+raygdev@users.noreply.github.com>
2022-10-09 02:47:16 -0400

    Modify repiar order schema and functions
    modified repair order schema to set notes to
    default and change vin to uppercase starting
    
    created functions to find repair order by user
    id and creaete a repiar order

--- server/models/repairOrderModel.js
+++ server/models/repairOrderModel.js
@@ -6,7 +6,7 @@ const RepairOrderSchema = new mongoose.Schema({
   tech_id: { type: String, required: true },
   vin: {
     type: String,
-    lowercase: true,
+    uppercase: true,
     trim: true,
     validate: {
       validator: function (v) {
@@ -17,6 +17,33 @@ const RepairOrderSchema = new mongoose.Schema({
     required: [true, "vin is required, must be 17 characters"],
   },
   created_on: { type: Date, default: new Date() },
+  notes: {type: String, default: ''}
 });
 
+//create a model for the repair order
+const RepairOrder = mongoose.model('RepairOrder',RepairOrderSchema,'RepairOrder')
+
 module.exports = RepairOrderSchema;
+
+exports.findUserRepairOrders = function (userId, done){
+  RepairOrder.find({tech_id: userId}, (err, repairOrders) =>{
+    if(err){
+      return done(err)
+    } else if(!repairOrders){
+      return done(null, false)
+    } else {
+      done(null, repairOrders)
+    }
+  })
+}
+
+exports.createRepairOrder = function (repairOrderObject,done){
+  const repairOrder = new RepairOrder(repairOrderObject)
+  repairOrder.save((err) => {
+    if(err){
+      return done(err)
+    }else {
+      done(null, user)
+    }
+  })
+}
