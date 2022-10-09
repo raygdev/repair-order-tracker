b2521081ec452d0bb2d709bc62f2fb0081b5b216
raygdev <101662915+raygdev@users.noreply.github.com>
2022-10-09 01:22:37 -0400

    Add dependencies and move files

--- server/controllers/userLoginController.js
+++ server/controllers/userLoginController.js
@@ -1,21 +1,23 @@
-const userModel = require('../models/userModel')
+const userModel = require("../models/userModel");
+const passport = require('passport-local')
 
-exports.userLoginController = (req,res,next) =>{
-    const {email, password} = req.body
-    userModel.findUserByEmail({email:email},(err,user) => {
-        if(err){
-            res.send(err)
-            return
-        }else if(!user){
-            res.status(404).send(`can't seem to find that user`)
-            return
-        } else if((password !== user.password)) {
-            res.status(404).send(`username/password combination doesn't exist`)
-            return
-        } else {
-            //toString() for ObjectId
-            res.status(200).send(user)
-            return
-        }
-    })
-}
+
+exports.userLoginController = (req, res, next) => {
+  const { email, password } = req.body;
+  userModel.findUserByEmail({ email: email }, (err, user) => {
+    if (err) {
+      res.send(err);
+      return;
+    } else if (!user) {
+      res.status(404).send(`can't seem to find that user`);
+      return;
+    } else if (password !== user.password) {
+      res.status(404).send(`username/password combination doesn't exist`);
+      return;
+    } else {
+      //toString() for ObjectId
+      res.status(200).send(user);
+      return;
+    }
+  });
+};
--- server/routes/register.js
+++ server/routes/register.js
@@ -1,14 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-const userModel = require("../models/userModel.js");
-const { handleRegistrationError } = require("../uitls/utils.js");
+const userRegistration = require('../controllers/userRegisterController')
 
-router.post("/api/register/user", (req, res, next) => {
-  return userModel.createAndSaveUser(req.body, (err, newUser) => {
-      if(err) return handleRegistrationError(res, err)
-      return res.status(200).send({ message: "ok", you: newUser });
-  });
-});
+router.post("/api/register/user",userRegistration.userRegitrationController );
 
 module.exports = router;
