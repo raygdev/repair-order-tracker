85e6cb3683e8e8fa8ae5d9c6721b9d5292cd691f
raygdev <101662915+raygdev@users.noreply.github.com>
2022-10-09 18:35:47 -0400

    Use repairOrder crud operations

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,11 +1,47 @@
+const roModel = require("../models/repairOrderModel.js");
 
+exports.getAllROs = (req, res, next) => {
+  let userId = req.body.userId;
+  let errorMessage = { message: `No RO's found for this user` };
+  roModel.findUserRepairOrders(userId, (err, repairOrders) => {
+    if (err) {
+      return res.status(404).json(errorMessage);
+    } else if (!repairOrders) {
+      return res.status(404).json(errorMessage);
+    } else {
+      return res.status(200).json(repairOrders);
+    }
+  });
+};
 
-exports.getAllROs = (req,res,next) => {}
+exports.getROsByDate = (req, res, next) => {};
 
-exports.getROsByDate = (req,res,next) => {}
+exports.updateOneRO = (req, res, next) => {};
 
-exports.updateOneRO = (req,res,next) => {}
+exports.deleteOneRO = (req, res, next) => {
+  let ro_id = req.body.ro_id;
+  let errorMessage = { message: "Something went wrong deleting the RO" };
+  roModel.deleteOneRepairOrderById(ro_id, (err, doc) => {
+    if (err) {
+      return res.status(404).json(errorMessage);
+    } else if (!doc) {
+      return res.status(404).json({ message: "Could not find that RO" });
+    } else {
+      return res
+        .status(200)
+        .json({ message: `Deleted RO number ${doc.ro_number} successfully!` });
+    }
+  });
+};
 
-exports.deleteOneRO = (req,res,next) => {}
-
-exports.createOneRO = (req,res,next) => {}
+exports.createOneRO = (req, res, next) => {
+  let errorMessage = { message: "Something went wrong saving the RO" };
+  roModel.createRepairOrder(req.body, (err, saved) => {
+    let successMessage = { message: `Successfuly saved the RO` };
+    if (err) {
+      return res.status(404).json(errorMessage);
+    } else {
+      return res.status(200).json(successMessage);
+    }
+  });
+};
