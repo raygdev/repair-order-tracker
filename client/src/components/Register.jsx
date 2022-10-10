4696e5d4ef27a908d2780547dc7ca37f928d6b25
raygdev <101662915+raygdev@users.noreply.github.com>
2022-10-09 22:32:05 -0400

    Refactor Register.jsx
    refactor to use react-router form and
    action to register a new user.

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,21 +1,18 @@
 import React from "react";
 import "../css/registerForm.css";
-import { Form } from 'react-router-dom'
+import { Form, redirect } from "react-router-dom";
 import { registerSubmission } from "../utils/registerSubmit";
 
 export const Register = (props) => {
-
   const buttonStyle = {
     backgroundColor: "yellow",
   };
-  
-
 
   return (
-    <Form className="register-form" method='post' action='../login'>
+    <Form className="register-form" method="post" action="../register">
       <input
         type="text"
-        name='firstName'
+        name="firstName"
         required={true}
         placeholder="First Name"
       />
@@ -25,48 +22,56 @@ export const Register = (props) => {
         required={true}
         placeholder="Last Name"
       />
-      <input
-        type="text"
-        name="shop_name"
-        placeholder="Shop Name"
-      />
+      <input type="text" name="shop_name" placeholder="Shop Name" />
       <input
         type="email"
         name="email"
         required={true}
         placeholder="Enter your email"
       />
-      <input
-        type="password"
-        name="password"
-        placeholder="Create a Password"
-      />
-      <button style={buttonStyle}>
-         "Submit"
-      </button>
+      <input type="password" name="password" placeholder="Create a Password" />
+      <button style={buttonStyle}>"Submit"</button>
     </Form>
   );
 };
-export async function redirectOnRegisterSuccess({request}){
-       const user =  await request.body
-       console.log(user)
-       return
-       
+export async function redirectOnRegisterSuccess({ request }) {
+  const user = await request.body;
+  console.log(user);
+  return;
 }
 
-export async function handleAction({request}){
-  const user = await request.formData()
-  // console.log(user)
-  const newUser={
-    name:{
-      first:user.get('firstName'),
-      last:user.get('lastName')
+export async function handleAction({ request }) {
+  const formData = await request.formData();
+  const user = Object.fromEntries(formData);
+  console.log(user);
+  const newUser = {
+    name: {
+      first: user.firstName,
+      last: user.lastName,
     },
-    email:user.get('email'),
-    password:user.get('password'),
-    shop_name:user.get('shop_name')
-  }
-  console.log(JSON.stringify(newUser))
-  return newUser
+    email: user.email,
+    password: user.password,
+    shop_name: user.shop_name,
+  };
 
+  return createNewUser(newUser).catch((e) => console.log(e.message));
+}
+
+async function createNewUser(userObj) {
+  const res = await fetch("/api/register/user", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(userObj),
+  });
+
+  if (!res.ok) {
+    console.log(res.message ?? res.message);
+    throw await res.json();
+    redirect("/register");
+  }
+
+  const newUser = await res.json();
+  return redirect("/login");
 }
--- client/src/router.js
+++ client/src/router.js
@@ -11,7 +11,6 @@ export const router = createBrowserRouter([
             {
                 path:'register',
                 element: <Register />,
-                loader: redirectOnRegisterSuccess,
                 action: handleAction,
             },
             {
