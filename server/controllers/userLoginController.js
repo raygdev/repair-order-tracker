d82b40227056eb1650c42fb1a822377b5ed78980
raygdev <101662915+raygdev@users.noreply.github.com>
2022-10-12 22:18:30 -0400

    Change response type to json
    providing route response messages for the
    catch block.

--- server/controllers/userLoginController.js
+++ server/controllers/userLoginController.js
@@ -11,10 +11,10 @@ exports.userLoginController = (req, res, next) => {
       res.send(err);
       return;
     } else if (!user) {
-      res.status(404).send(`can't seem to find that user`);
+      res.status(404).json({message:`can't seem to find that user`});
       return;
     } else if (password !== user.password) {
-      res.status(404).send(`username/password combination doesn't exist`);
+      res.status(404).json({message:`username/password combination doesn't exist`});
       return;
     } else {
       res.status(200).json(user);
