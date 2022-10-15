f570ae72da4ffcc17a498f85a28b529cd63506d3
raygdev <101662915+raygdev@users.noreply.github.com>
2022-10-15 00:50:55 -0400

    Make the header sticky

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 export const Header = (props) => {
   return (
-    <header className='p-4 bg-sky-500 flex'>
+    <header className='p-4 bg-sky-500 flex sticky top-0'>
         <nav className='bg-sky-500 flex basis-full justify-around'>
             <Link to='login' className='text-white'>Login</Link>
             <Link to='register'className='text-white'>Register</Link>
