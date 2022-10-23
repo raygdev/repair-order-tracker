fff50f6e452cb66c07397f6944e897457fc2940d
raygdev <101662915+raygdev@users.noreply.github.com>
2022-10-23 19:32:36 -0400

    Conditonally render Create button

--- client/src/pages/RepairOrders.js
+++ client/src/pages/RepairOrders.js
@@ -3,14 +3,18 @@ import { Link } from "react-router-dom";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import { faLink, faLongArrowAltRight } from '@fortawesome/free-solid-svg-icons'
 import DeleteButton from "../components/DeleteButton";
+import CreateRepairButton from "../components/CreateRepairButton";
 
 export default function RepairOrders({ repairOrders, userId }) {
+
+  const createButton = repairOrders.length ? <CreateRepairButton />: null
+
   const orders =
     repairOrders.length &&
     repairOrders.map((ro) => {
       return (
         <Fragment key={ro._id}>
-          <li className=" flex justify-around text-white">
+          <li className=" flex justify-between text-white">
             <div>
             <FontAwesomeIcon icon={faLongArrowAltRight}/> {ro.ro_number} 
             </div>
@@ -18,11 +22,13 @@ export default function RepairOrders({ repairOrders, userId }) {
         </Fragment>
       );
     });
+    
   return (
     <div className="bg-sky-700 h-screen flex flex-col align-center mt-6 p-6 m-auto w-3/4 rounded-lg md:w-2/4">
       <h2 className="text-xl font-bold  text-white self-center">
         Repair Orders
       </h2>
+      {createButton}
       {repairOrders.length ? (
         <ul>{orders}</ul>
       ) : (
