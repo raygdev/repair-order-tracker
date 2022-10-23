ab9cd9d3b99a964bd712e3328c597ec3d1e3344a
raygdev <101662915+raygdev@users.noreply.github.com>
2022-10-23 19:42:33 -0400

    Add home link to nav

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,6 +5,7 @@ export const Header = (props) => {
   return (
     <header className='p-4 bg-sky-500 flex sticky top-0'>
         <nav className='bg-sky-500 flex basis-full justify-around'>
+            <Link to='/' className='text-white'>Home</Link>
             <Link to='login' className='text-white'>Login</Link>
             <Link to='register'className='text-white'>Register</Link>
             <Link to='about'className='text-white'>About</Link>
