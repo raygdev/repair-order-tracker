822a8b9155709bbab23a0520904e6dd684d51644
raygdev <101662915+raygdev@users.noreply.github.com>
2022-10-23 18:07:15 -0400

    Add a path and action for delete
    add a path on router for repair order
    deletion import and use action

--- client/src/router.js
+++ client/src/router.js
@@ -7,6 +7,7 @@ import User, { userLoaderFunction } from "./pages/User";
 import CreateRepairOrder, {createROActionLoader} from "./pages/CreateRepairOrder";
 import NotFound from "./pages/NotFound";
 import Home from "./pages/Home";
+import { deleteRepairOrderAction } from "./components/DeleteButton";
 export const router = createBrowserRouter([
     {
         path:'/',
@@ -42,6 +43,10 @@ export const router = createBrowserRouter([
                      path:'create-repair-order',
                      action:createROActionLoader,
                      element: <CreateRepairOrder />
+                    },
+                    {
+                        path:'repairorder/delete/:repairId',
+                        action: deleteRepairOrderAction
                     }
                 ]
             }
