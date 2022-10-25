02074b49b5a25533793f195cec3fbcac29cee9eb
raygdev <101662915+raygdev@users.noreply.github.com>
2022-10-25 19:58:06 -0400

    Provide responsive layout

--- client/src/pages/CreateRepairOrder.jsx
+++ client/src/pages/CreateRepairOrder.jsx
@@ -8,8 +8,9 @@ import { faExternalLink } from "@fortawesome/free-solid-svg-icons";
 
 export default function CreateRepairOrder () {
   return (
-    <div>
-      <Form className="p-4 bg-sky-500 flex flex-col h-80 w-1/3 justify-between my-0 mx-auto rounded shadow-lg shadow-slate-500" action="../create-repair-order" method="POST">
+    <div className="flex min-h-screen">
+      <Form className="p-4 bg-sky-500 flex flex-col sm:h-5/6 min-h-[450px] sm:w-2/4 w-4/6 md:w-1/3 justify-between m-auto rounded shadow-lg shadow-slate-500" action="../create-repair-order" method="POST">
+        <div>
         <label htmlFor="ro_number" className="text-white font-medium">
           RO Number*
         </label>
@@ -18,20 +19,25 @@ export default function CreateRepairOrder () {
           name="ro_number"
           type="number"
           placeholder="Enter Your RO Number "
-          className="p-1 rounded"
+          className="p-1 rounded w-full"
         />
+        </div>
+        <div>
         <label htmlFor="vin" className="text-white font-medium">
           VIN*
         </label>
-        <input className="p-1 rounded" id="vin" name="vin" type="text" placeholder="Enter your VIN" />
+        <input className="p-1 rounded w-full" id="vin" name="vin" type="text" placeholder="Enter your VIN" />
+        </div>
         <div className="flex w-2/4 justify-between items-center">
         <label  htmlFor="isWarranty" className="text-white font-medium">Warranty</label>
         <input type='checkbox' id='isWarranty' name='isWarranty'/>
         </div>
+        <div>
         <label htmlFor="notes" className="text-white font-medium">
           Notes
         </label>
-        <textarea className="p-1 rounded" id="notes" name="notes" rows={5} cols={5} />
+        <textarea className="p-1 rounded w-full" id="notes" name="notes" rows={5} cols={5} />
+        </div>
         <button className="text-white rounded bg-sky-800 mt-5 w-3/4 self-center px-1 py-3 hover:bg-sky-400" type="submit">Submit <FontAwesomeIcon icon={faExternalLink}/></button>
       </Form>
     </div>
