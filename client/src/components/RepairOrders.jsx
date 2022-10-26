cac0546ff65a8a3748effe855522c7506d155da0
raygdev <101662915+raygdev@users.noreply.github.com>
2022-10-25 20:39:29 -0400

    Move RepairOrders to components folder

---
+++ client/src/components/RepairOrders.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import CreateRepairButton from "./CreateRepairButton";
+import RepairOrderListItems from "./RepairOrderListItems";
+import CreateFirstRepair from "./CreateFirstRepair";
+
+export default function RepairOrders({ repairOrders, userId }) {
+
+  const sortedOrders =
+    repairOrders.length &&
+    repairOrders
+      .sort((a, b) => {
+        if (a._id > b._id) {
+          return -1;
+        }
+      })
+      .map((ro) => {
+        return (
+          <RepairOrderListItems
+            key={ro._id}
+            id={ro._id}
+            ro_number={ro.ro_number}
+          />
+        );
+      });
+
+  return (
+    <div className="bg-sky-700 h-screen flex flex-col align-center mt-6 p-6 m-auto w-3/4 rounded-lg md:w-2/4">
+      <h2 className="text-xl font-bold  text-white self-center">
+        Repair Orders
+      </h2>
+      {sortedOrders.length && <CreateRepairButton />}
+      {sortedOrders.length ? (
+        <ul>{sortedOrders}</ul>
+      ) : (
+        <CreateFirstRepair userId={userId} />
+      )}
+    </div>
+  );
+}
