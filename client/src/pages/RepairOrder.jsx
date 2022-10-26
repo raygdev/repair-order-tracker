a62c25300f49cc6f3ab337cf2edd7e8c95d097f3
raygdev <101662915+raygdev@users.noreply.github.com>
2022-10-25 20:42:41 -0400

    Provide basic layout

--- client/src/pages/RepairOrder.jsx
+++ client/src/pages/RepairOrder.jsx
@@ -15,14 +15,16 @@ export default function RepairOrder() {
     let date = new Date(repairOrder.created_on).toLocaleDateString()
 
   return (
-    <main className='flex flex-col bg-sky-500 items-center text-white p-4 my-5 md:w-3/4 m-auto lg:w-2/4'>
+    <main className='flex flex-col bg-sky-500 items-center text-white p-4 mx-auto mt-5 md:w-3/4 lg:w-2/4'>
         <div className='flex justify-between w-96 p-4'>
             <p title='repair order number' className=''>Repair Order # {repairOrder.ro_number}</p>
-            <p title='repair order creation date' className=''>Created On <date>{date}</date></p>
+            <p title='repair order creation date' date={date} className=''>Created On {date}</p>
         </div>
         <p title='vehicle vin number'>VIN # {repairOrder.vin}</p>
         <p title='pay type' className='whitespace-pre-wrap'>Pay Type: {repairOrder.isWarranty ? "Warranty":"Customer Pay"}</p>
-        <p title='technician notes' className='self-start whitespace-pre-line w-full'>Tech Notes: <br /><hr/>{repairOrder.notes}</p>
+        <hr className='w-full my-4'/>
+        <p title='technician notes' className='self-start whitespace-pre-line w-full'>Tech Notes: <br />{repairOrder.notes}</p>
+        <hr className='w-full my-4'/>
         <Link to={`/user/${userId}`} className='underline hover:text-violet-900'>Go to Dashboard</Link>
         <button>Edit <FontAwesomeIcon icon={faPenToSquare} /></button>
     </main>
