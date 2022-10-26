39d5f7d5de35f6fe3691b273d603522a56fe90d6
raygdev <101662915+raygdev@users.noreply.github.com>
2022-10-25 20:49:03 -0400

    Reformat file

--- client/src/components/RepairOrderListItems.jsx
+++ client/src/components/RepairOrderListItems.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLongArrowAltRight } from '@fortawesome/free-solid-svg-icons'
-import DeleteButton from './DeleteButton'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLongArrowAltRight } from "@fortawesome/free-solid-svg-icons";
+import DeleteButton from "./DeleteButton";
+import { Link } from "react-router-dom";
 
 export default function RepairOrderListItems(props) {
   return (
     <>
-    <li className=" flex justify-between text-white text-lg">
-      <Link to={`repairorder/${props.id}`}>
-      <FontAwesomeIcon icon={faLongArrowAltRight}/> {props.ro_number} 
-      </Link>
-      <DeleteButton id={props.id}/></li>
-  </>
-  )
+      <li className=" flex justify-between text-white text-lg">
+        <Link to={`repairorder/${props.id}`}>
+          <FontAwesomeIcon icon={faLongArrowAltRight} /> {props.ro_number}
+        </Link>
+        <DeleteButton id={props.id} />
+      </li>
+    </>
+  );
 }
