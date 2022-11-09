ebd41a7eafeed2361abd19787021237a7b5dbf90
raygdev <101662915+raygdev@users.noreply.github.com>
2022-11-09 18:32:26 -0500

    Reformat user login controller

--- server/controllers/userLoginController.js
+++ server/controllers/userLoginController.js
@@ -7,16 +7,13 @@ exports.userLoginController = (req, res, next) => {
   const { email, password } = req.body;
 
   userModel.findUserByEmail({ email: email }, (err, user) => {
-    if (err) {
-      return res.send(err);
-    } else if (!user) {
-      return res.status(404).json({ message: `can't seem to find that user` });
-    } else if (!bcrypt.compareSync(password, user.password)) {
-      return res
-        .status(404)
-        .json({ message: `username/password combination doesn't exist` });
-    } else {
-      return res.status(200).json({...user._doc, password:null});
-    }
+
+    if (err) return res.send(err);
+
+    if (!user) return res.status(404).json({ message: `can't seem to find that user` });
+
+    if (!bcrypt.compareSync(password, user.password)) return res.status(404).json({ message: `username/password combination doesn't exist` });
+    
+    return res.status(200).json({...user._doc, password:null});
   });
 };
