2146cdb0bd3e6bac50c6ae2e0dc637ffc89bce89
raygdev <101662915+raygdev@users.noreply.github.com>
2022-12-05 21:15:12 -0500

    Add section and dynamically render
    adds a section and dynamically renders icons and
    highlights from highlightsData.js

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import loadable from '@loadable/component'
+import highlightsData from '../utils/highlightsData'
 const Link = loadable(() => import('react-router-dom').then(module => ({default:module.Link})))
+const FontAwesomeIcon = loadable(() => import('@fortawesome/react-fontawesome').then(module => ({default: module.FontAwesomeIcon})))
 
 export default function Home() {
   return (
-        <main className='min-h-screen w-full flex flex-col justify-center tracking-widest text-center bg-gradient-to-br from-blue-500 via-sky-300 p-10 shadow-lg shadow-slate-600'>
+    <>
+        <main className='min-h-screen w-full flex flex-col justify-center tracking-widest text-center bg-gradient-to-br from-blue-500 via-sky-300 p-10 '>
             <div className='w-3/4 text-center self-center'>
                 <h1 className='md:text-5xl uppercase py-8 font-extrabold text-2xl'>Welcome to the RO Tracker!</h1>
                 <p className='md:text-lg text-sm font-semibold uppercase'>This is a free service without ads for auto techs by auto techs!</p>
@@ -14,25 +17,31 @@ export default function Home() {
                 <div className='flex flex-col py-8 items-center justify-between'>
                     <p className='py-3 '>Already have an <span className='font-bold'>account</span>?</p>
                     <hr className='text-slate-500'/>
-                    <Link to='/login' className='text-white font-semibold inline-block px-4 py-1 bg-sky-700 rounded shadow-md hover:bg-sky-900  active:bg-sky-500 focus:outline-none focus:ring-4 focus:ring-sky-400 transition-all' data-testid='home-login'>Login</Link>
+                    <Link to='/login' className='text-white font-semibold inline-block px-8 py-1 bg-sky-700 rounded shadow-md hover:bg-sky-900  active:bg-sky-500 focus:outline-none focus:ring-4 focus:ring-sky-400 transition-all' data-testid='home-login'>Login</Link>
                 </div>
 
                 <div className='flex flex-col justify-between px-8 items-center'>
                     <p className='py-3'><span className='font-bold'>Or</span></p>
-                    <Link to='/register' className='text-white justify-end font-semibold inline-block px-4 py-1 bg-sky-700 rounded shadow-md hover:bg-sky-900 active:bg-sky-500 focus:outline-none focus:ring-4 focus:ring-sky-400 transition-all'>Register</Link>
+                    <Link to='/register' className='text-white justify-end font-semibold inline-block px-5 py-1 bg-sky-700 rounded shadow-md hover:bg-sky-900 active:bg-sky-500 focus:outline-none focus:ring-4 focus:ring-sky-400 transition-all'>Register</Link>
                 </div>
             </div>
-            <section className='w-full bg-green-300 px-5 py-6 mt-10  grid grid-rows-2 grid-flow-col items-center justify-center'>
-                <div className='col-start-1 col-span-2'>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto ullam ex itaque debitis aspernatur molestias, quisquam adipisci quam natus tempora.</p>
-                </div>
-                <div className='col-span-1'>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis autem enim eligendi? Quae, quas illo? Cum officia doloremque voluptatum, voluptas delectus adipisci debitis quis recusandae culpa reiciendis veniam omnis blanditiis.</p>
-                </div>
-            </section>
-            <section className=''>
-
-            </section>
+           
         </main>
+        <section className=' my-5 p-4 w-full min-h-screen grid gap-y-4 gap-x-8  xs:grid-cols-2 md:grid-cols-3 text-center justify-center'>
+           {highlightsData.map(highlights => {
+                const  {id, icon, highlight} = highlights
+                return (
+                    <div key={id} className='border rounded p-4 flex flex-col justify-evenly shadow-md shadow-slate-500 '>
+                        < FontAwesomeIcon className='text-4xl text-violet-500' icon={icon}/>
+                        <p>{highlight}</p>
+                    </div>
+                )
+           }) }
+        </section>
+
+        <section className=''>
+
+        </section>
+    </>
   )
 }
---
+++ client/src/utils/highlightsData.js
@@ -0,0 +1,37 @@
+import {faBookOpen, faCar, faChartLine, faDatabase, faStickyNote, faToolbox} from '@fortawesome/free-solid-svg-icons'
+
+
+const highlights = [
+    {
+        id:1,
+        icon: faStickyNote,
+        highlight:'Take notes to keep track of and highlight your personal insights related to the repair'
+    },
+    {
+        id:2,
+        icon: faCar,
+        highlight:'Input the VIN or RO number for the vehicle to keep track of previous repairs performed so that you have all the information you need in one spot'
+    },
+    {
+        id:3,
+        icon: faChartLine,
+        highlight:'Track your labor hours and total time sold, and unsold, to know where you stand in your hours throughout the week.'
+    },
+    {
+        id:4,
+        icon: faBookOpen,
+        highlight:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis autem enim eligendi? Quae, quas illo? Cum officia doloremque voluptatum'
+    },
+    {
+        id:5,
+        icon: faDatabase,
+        highlight:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis autem enim eligendi? Quae, quas illo? Cum officia doloremque voluptatum'
+    },
+    {
+        id:6,
+        icon: faToolbox,
+        highlight:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis autem enim eligendi? Quae, quas illo? Cum officia doloremque voluptatum'
+    }
+]
+
+export default highlights
