10db8ba35584f3845d708b2d9f7aeb154ff5e5c4
raygdev <101662915+raygdev@users.noreply.github.com>
2022-12-05 21:23:12 -0500

    Fix misspelling of aria-label

--- client/src/components/DeleteButton.jsx
+++ client/src/components/DeleteButton.jsx
@@ -10,7 +10,7 @@ export default function DeleteButton(props) {
   return (
     <>
         <Form method='delete' action={`${props.path}${props.id}`}>
-            <button area-label='delete' type='submit' className='transition ease-in-out delay-100 hover:text-red-400 font-semibold'>{buttonText}<FontAwesomeIcon size={props.size} icon={faTrashCan} /></button>
+            <button aria-label='delete' type='submit' className='transition ease-in-out delay-100 hover:text-red-400 font-semibold'>{buttonText}<FontAwesomeIcon size={props.size} icon={faTrashCan} /></button>
         </Form>
     </>
   )
