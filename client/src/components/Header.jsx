e42657e2bddd64e44017a1fbeaef3fc39ee6e3de
raygdev <101662915+raygdev@users.noreply.github.com>
2022-12-07 22:57:03 -0500

    Move classes to parent for inheritance
    moves classes to parent for inheritance and
    reduces repetition

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,12 +5,12 @@ export default function Header(props) {
  const location = useLocation()
  console.log(location.pathname)
   return (
-    <header className='p-4 bg-sky-700 flex sticky top-0 min-w-max'>
+    <header className='p-4 bg-sky-700 flex sticky top-0 min-w-max text-white font-semibold'>
         <nav className='flex basis-full justify-around'>
-            <Link to='/' className={`text-white font-semibold p-4 ${location.pathname === '/' ? 'underline underline-offset-4' : ''}`}>Home</Link>
-            <Link to='login' className={`text-white font-semibold p-4 ${location.pathname === '/login' ? 'underline underline-offset-4' : ''}`}>Login</Link>
-            <Link to='register'className={`text-white font-semibold p-4 ${location.pathname === '/register' ? 'underline underline-offset-4' : ''}`}>Register</Link>
-            <Link to='about'className={`text-white font-semibold p-4 ${location.pathname === '/about' ? 'underline underline-offset-4' : ''}`}>About</Link>
+            <Link to='/' className={`p-4 ${location.pathname === '/' ? 'underline underline-offset-4' : ''}`}>Home</Link>
+            <Link to='login' className={`p-4 ${location.pathname === '/login' ? 'underline underline-offset-4' : ''}`}>Login</Link>
+            <Link to='register'className={`p-4 ${location.pathname === '/register' ? 'underline underline-offset-4' : ''}`}>Register</Link>
+            <Link to='about'className={`p-4 ${location.pathname === '/about' ? 'underline underline-offset-4' : ''}`}>About</Link>
         </nav>
     </header>
   )
