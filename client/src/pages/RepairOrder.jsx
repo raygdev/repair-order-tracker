1f28c34f8b26e6bd8cb059b13cc99007aae9bc31
raygdev <101662915+raygdev@users.noreply.github.com>
2022-12-11 15:14:39 -0500

    Add semantics and remove comment

--- client/src/pages/RepairOrder.jsx
+++ client/src/pages/RepairOrder.jsx
@@ -5,7 +5,6 @@ import loadable from '@loadable/component'
 const DeleteButton = loadable(() => import('../components/DeleteButton'))
 const FontAwesomeIcon = loadable(() => import('@fortawesome/react-fontawesome').then(module => ({default:module.FontAwesomeIcon})))
 
-//TODO pretty up the UI
 
 export default function RepairOrder() {
     useEffect(() => {
@@ -15,26 +14,28 @@ export default function RepairOrder() {
     const { repairOrders } = useRouteLoaderData('root')
     const { repairId, userId } = useParams()
 
-    const [repairOrder ] = repairOrders.filter(RO => RO._id === repairId)
+    const repairOrder  = repairOrders.find(RO => RO._id === repairId)
 
     let date = new Date(repairOrder.created_on).toLocaleDateString()
 
   return (
-    <main className='flex flex-col bg-sky-500 items-center text-white p-4 mx-auto mt-5 md:w-3/4 lg:w-2/4'>
-        <div className='flex justify-between w-96 p-4'>
-            <p title='repair order number' className=''>Repair Order # {repairOrder.ro_number}</p>
-            <p title='repair order creation date' date={date} className=''>Created On {date}</p>
-        </div>
-        <p title='vehicle vin number'>VIN # {repairOrder.vin}</p>
-        <p title='pay type' className='whitespace-pre-wrap'>Pay Type: {repairOrder.isWarranty ? "Warranty":"Customer Pay"}</p>
-        <hr className='w-full my-4'/>
-        <p title='technician notes' className='self-start whitespace-pre-line w-full'>Tech Notes: <br />{repairOrder.notes}</p>
-        <hr className='w-full my-4'/>
-        <div className='flex w-full justify-between'>
+    <main className='bg-sky-500 items-center text-white p-4 mt-5 mx-auto min-h-max xs: max-w-xl'>
+        <section className='text-center mb-10'>
+            <p>Repair Order # {repairOrder.ro_number}</p>
+            <p date={date}>Created On {date}</p>
+        </section>
+        <section className='border-b text-center py-4 mb-10'>
+            <p title='vehicle vin number'>VIN # {repairOrder.vin}</p>
+            <p title='pay type' className='whitespace-pre-wrap'>Pay Type: {repairOrder.isWarranty ? "Warranty":"Customer Pay"}</p>
+        </section>
+        <section className='w-full border-b py-4 mb-10'>
+            <p title='technician notes' className='whitespace-pre-line w-full'>Tech Notes: <br />{repairOrder.notes}</p>
+        </section>
+        <section className='flex w-full justify-between py-4'>
             <Link to={`/user/${userId}`} className='underline hover:text-violet-900'>Go to Dashboard</Link>
             <DeleteButton path={'../repairorder/delete/'}  id={repairOrder._id} text={'Delete'}/>
             <Link to={`../editrepairorder/${repairId}`} className='transition ease-in-out delay-100 hover:text-green-300 font-semibold'>Edit <FontAwesomeIcon icon={faPenToSquare} /></Link>
-        </div>
+        </section>
     </main>
   )
 }
