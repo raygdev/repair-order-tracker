f984832200138159cb47598382c564c025548e4c
raygdev <101662915+raygdev@users.noreply.github.com>
2022-12-11 15:07:17 -0500

    Use ternary in sort and remove comments

--- client/src/components/RepairOrderListItems.jsx
+++ client/src/components/RepairOrderListItems.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLongArrowAltRight } from "@fortawesome/free-solid-svg-icons";
-// import DeleteButton from "./DeleteButton";
 import { Link } from "react-router-dom";
 import loadable from "@loadable/component";
 const DeleteButton = loadable(() => import('./DeleteButton'))
