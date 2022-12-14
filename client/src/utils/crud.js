73ef5ec86137fa2f55ac69dcae6b15313c3ebf5d
raygdev <101662915+raygdev@users.noreply.github.com>
2022-12-13 20:57:06 -0500

    Add token to localStorage
    adds token to localStorage must provide
    store for token later
    
    sends token in auth header to api
    for protected routes.
    
    reformats file

--- client/src/utils/crud.js
+++ client/src/utils/crud.js
@@ -1,90 +1,105 @@
 import { redirect } from "react-router-dom";
+const token = localStorage.getItem("token");
 
 export async function verifyUser(userObject) {
-    const res = await fetch("/api/login", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(userObject),
-    });
-  
-    if (!res.ok) {
-      throw await res.json();
-    }
-    const user = await res.json();
-  
-    return redirect(`/user/${user._id}`);
+  const res = await fetch("/api/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(userObject),
+  });
+
+  if (!res.ok) {
+    throw await res.json();
   }
+  const user = await res.json();
+
+  console.log(user.token);
+
+  localStorage.setItem("token", user.token);
+
+  return redirect(`/user/${user._id}`);
+}
 
 export async function createNewUser(userObj) {
-    const res = await fetch("/api/register/user", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(userObj),
-    });
-  
-    if (!res.ok) {
-      throw await res.json();
-    }
-  
-    console.log('new user created successfully')
-    return redirect("/login");
+  const res = await fetch("/api/register/user", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(userObj),
+  });
+
+  if (!res.ok) {
+    throw await res.json();
   }
 
-export async function deleteRepairOrder(ids){
-    
-    const res = await fetch('/repairorder',{
-        method: 'delete',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(ids)
-    })
+  console.log("new user created successfully");
+  return redirect("/login");
+}
 
-    if (!res.ok){
-        throw res
-    }
+export async function deleteRepairOrder(ids) {
+  const res = await fetch("/repairorder", {
+    method: "delete",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(ids),
+  });
 
-    return redirect(`/user/${ids.userId}`)
+  if (!res.ok) {
+    throw res;
+  }
+
+  return redirect(`/user/${ids.userId}`);
 }
 
 export async function createRO(ro, userId) {
-    const res = await fetch("/repairorder", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(ro),
-    });
-  
-    if (!res.ok) {
-      throw await res.json();
-    }
-    return redirect(`/user/${userId}`);
+  const res = await fetch("/repairorder", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(ro),
+  });
+
+  if (!res.ok) {
+    throw await res.json();
   }
-
-export async function editRO(updatedRO, userId){
-    const res = await fetch('/repairorder',{
-        method:'put',
-        headers:{
-            "Content-Type":"application/json"
-        },
-        body: JSON.stringify(updatedRO)
-    })
-
-    if(!res.ok){
-        throw res
-    }
-
-    return redirect(`/user/${userId}/repairorder/${updatedRO._id}`)
+  return redirect(`/user/${userId}`);
 }
 
-export async function getUser(userId){
-    const res = await fetch(`/api/user/${userId}`)
-    if(!res.ok){
-        throw await res.json()
-    }
-    const user = await res.json()
-    return user
+export async function editRO(updatedRO, userId) {
+  const res = await fetch("/repairorder", {
+    method: "put",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(updatedRO),
+  });
+
+  if (!res.ok) {
+    throw res;
+  }
+
+  return redirect(`/user/${userId}/repairorder/${updatedRO._id}`);
+}
+
+export async function getUser(userId) {
+  const res = await fetch(`/api/user/${userId}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  if (res.status === 401) {
+    return redirect("/login");
+  }
+  if (!res.ok) {
+    throw await res.json();
+  }
+
+  const user = await res.json();
+  return user;
 }
