f502fe3992ee6e64f6aa5e85e7916eb347264167
raygdev <101662915+raygdev@users.noreply.github.com>
2023-01-29 01:27:46 -0500

    Reformat AddRepairOrderButton.js

--- client/src/components/AddRepairButton.jsx
+++ client/src/components/AddRepairButton.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
-import { faAdd } from '@fortawesome/free-solid-svg-icons'
-import loadable from '@loadable/component'
-const Link = loadable(() => import('react-router-dom').then(module => ({default:module.Link})))
-const FontAwesomeIcon = loadable(() => import('@fortawesome/react-fontawesome').then(module => ({default:module.FontAwesomeIcon})))
+import React from "react";
+import { faAdd } from "@fortawesome/free-solid-svg-icons";
+import loadable from "@loadable/component";
+const Link = loadable(() =>
+  import("react-router-dom").then((module) => ({ default: module.Link }))
+);
+const FontAwesomeIcon = loadable(() =>
+  import("@fortawesome/react-fontawesome").then((module) => ({
+    default: module.FontAwesomeIcon,
+  }))
+);
 
 export default function AddRepairButton() {
   return (
-    <div className='flex justify-center py-5 underline font-semibold text-lg'>
-        <Link to='create-repair-order' className=' hover:text-green-300 transition-all'>Add <FontAwesomeIcon icon={faAdd} /></Link>
+    <div className="flex justify-center py-5 underline font-semibold text-lg">
+      <Link
+        to="create-repair-order"
+        className=" hover:text-green-300 transition-all"
+      >
+        Add <FontAwesomeIcon icon={faAdd} />
+      </Link>
     </div>
-  )
+  );
 }
