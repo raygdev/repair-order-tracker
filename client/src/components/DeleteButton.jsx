d7173c49ba2cd810d6d2f72251958fb30adccd1a
raygdev <101662915+raygdev@users.noreply.github.com>
2023-01-29 01:09:49 -0500

    Remove unused import and reformat file

--- client/src/components/DeleteButton.jsx
+++ client/src/components/DeleteButton.jsx
@@ -1,17 +1,29 @@
-import React from 'react'
-import loadable from '@loadable/component'
-import { redirect } from 'react-router-dom'
-import { faTrashCan } from '@fortawesome/free-solid-svg-icons'
-const Form = loadable(() => import('react-router-dom').then(module => ({default: module.Form})))
-const FontAwesomeIcon = loadable(() => import('@fortawesome/react-fontawesome').then(module => ({default:module.FontAwesomeIcon})))
+import React from "react";
+import loadable from "@loadable/component";
+import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
+const Form = loadable(() =>
+  import("react-router-dom").then((module) => ({ default: module.Form }))
+);
+const FontAwesomeIcon = loadable(() =>
+  import("@fortawesome/react-fontawesome").then((module) => ({
+    default: module.FontAwesomeIcon,
+  }))
+);
 
 export default function DeleteButton(props) {
-    const buttonText = props.text ? (props.text + ' ') : ''
+  const buttonText = props.text ? props.text + " " : "";
   return (
     <>
-        <Form method='delete' action={`${props.path}${props.id}`}>
-            <button aria-label='delete' type='submit' className='transition ease-in-out delay-100 hover:text-red-400 font-semibold'>{buttonText}<FontAwesomeIcon size={props.size} icon={faTrashCan} /></button>
-        </Form>
+      <Form method="delete" action={`${props.path}${props.id}`}>
+        <button
+          aria-label="delete"
+          type="submit"
+          className="transition ease-in-out delay-100 hover:text-red-400 font-semibold"
+        >
+          {buttonText}
+          <FontAwesomeIcon size={props.size} icon={faTrashCan} />
+        </button>
+      </Form>
     </>
-  )
+  );
 }
