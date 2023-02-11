23950037b6a561d2cadf82c83f70a310889e28d1
raygdev <101662915+raygdev@users.noreply.github.com>
2023-02-11 16:08:50 -0500

    Conditionally render nav based on route

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,22 +1,57 @@
-import React from 'react'
-import { Link, useLocation, useParams } from 'react-router-dom'
-import Logout from './Logout'
+import React from "react";
+import { Link, useLocation, useParams } from "react-router-dom";
+import Logout from "./Logout";
 
 export default function Header(props) {
- const { userId } = useParams()
- const location = useLocation()
- let toggleLogin = (!localStorage.getItem('token')) ? <Link to= 'login'className={`p-4 ${location.pathname === '/login' ? 'underline underline-offset-4' : ''}`}>Login</Link> : <Logout/>
- let toggleHome = (localStorage.getItem('token')) ? `user/${userId}` : '/'
-
+  const { userId } = useParams();
+  const location = useLocation();
+  let toggleLogin = !localStorage.getItem("token") ? (
+    <Link
+      to="login"
+      className={`p-4 ${
+        location.pathname === "/login" ? "underline underline-offset-4" : ""
+      }`}
+    >
+      Login
+    </Link>
+  ) : (
+    <Logout />
+  );
+  let toggleHome = localStorage.getItem("token") ? `user/${userId}` : "/";
 
   return (
-    <header className='p-4 bg-sky-700 flex sticky top-0 min-w-max text-white font-semibold'>
-        <nav className='flex basis-full justify-around'>
-            <Link to={toggleHome} className={`p-4 ${location.pathname === '/' ? 'underline underline-offset-4' : ''}`}>Home</Link>
-            {toggleLogin}
-            {!localStorage.getItem('token') && <Link to='register'className={`p-4 ${location.pathname === '/register' ? 'underline underline-offset-4' : ''}`}>Register</Link>}
-            <Link to='about'className={`p-4 ${location.pathname === '/about' ? 'underline underline-offset-4' : ''}`}>About</Link>
-        </nav>
+    <header className="p-4 bg-sky-700 flex sticky top-0 min-w-max text-white font-semibold">
+      <nav className="flex basis-full justify-around">
+        <Link
+          to={toggleHome}
+          className={`p-4 ${
+            location.pathname === "/" ? "underline underline-offset-4" : ""
+          }`}
+        >
+          Home
+        </Link>
+        {toggleLogin}
+        {!localStorage.getItem("token") && (
+          <Link
+            to="register"
+            className={`p-4 ${
+              location.pathname === "/register"
+                ? "underline underline-offset-4"
+                : ""
+            }`}
+          >
+            Register
+          </Link>
+        )}
+        <Link
+          to="about"
+          className={`p-4 ${
+            location.pathname === "/about" ? "underline underline-offset-4" : ""
+          }`}
+        >
+          About
+        </Link>
+      </nav>
     </header>
-  )
+  );
 }
