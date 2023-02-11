b02dddf848408e88737249ba721da4a3ac8b7025
raygdev <101662915+raygdev@users.noreply.github.com>
2023-02-11 16:06:48 -0500

    Refactor CreateFirstRepair.js

--- client/src/components/CreateFirstRepair.jsx
+++ client/src/components/CreateFirstRepair.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import loadable from "@loadable/component";
 import { faLink } from "@fortawesome/free-solid-svg-icons";
-const Link = loadable(() => import('react-router-dom').then(module => ({default:module.Link})))
-const FontAwesomeIcon = loadable(() => import('@fortawesome/react-fontawesome').then(module => ({default:module.FontAwesomeIcon})))
+const Link = loadable(() =>
+  import("react-router-dom").then((module) => ({ default: module.Link }))
+);
+const FontAwesomeIcon = loadable(() =>
+  import("@fortawesome/react-fontawesome").then((module) => ({
+    default: module.FontAwesomeIcon,
+  }))
+);
 
 export default function CreateFirstRepair({ userId }) {
   return (
