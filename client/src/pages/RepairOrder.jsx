f90d6ee678f966d4d39b431f06ea7377906e4666
raygdev <101662915+raygdev@users.noreply.github.com>
2023-06-25 21:56:01 -0400

    Add vehicle to page
    adds the vehicle information to the
    page based off of the vin
    
    changes the layout of the repair order
    to be a little less jarring

--- client/src/pages/RepairOrder.jsx
+++ client/src/pages/RepairOrder.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { useRouteLoaderData, useParams, Link } from 'react-router-dom'
+import { useRouteLoaderData, useLoaderData, useParams, Link } from 'react-router-dom'
 import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
 import loadable from '@loadable/component'
 import { toLocalDateString } from '../utils/datesHelpers'
@@ -13,6 +13,7 @@ export default function RepairOrder() {
     }, [])
 
     const { repairOrders } = useRouteLoaderData('root')
+    const data = useLoaderData()
     const { repairId, userId } = useParams()
 
     const repairOrder  = repairOrders.find(RO => RO._id === repairId)
@@ -20,19 +21,36 @@ export default function RepairOrder() {
     let date = toLocalDateString(repairOrder.created_on)
 
   return (
-    <main className='bg-sky-500 items-center text-white p-4 mt-5 mx-auto min-h-max xs: max-w-xl'>
-        <section className='text-center mb-10'>
-            <p>Repair Order # {repairOrder.ro_number}</p>
+    <main className='items-center pb-4 mt-5 mx-auto min-h-max xs: max-w-xl bg-[#fffff2] rounded shadow-lg'>
+        <div  className='shadow-md p-8 flex justify-around'>
+            <h2 className='text-lg text'>Repair Order# {repairOrder.ro_number}</h2>
             <p date={date}>Created On {date}</p>
+        </div>
+        { data && typeof data !== "string" &&
+            <section className='border-b p-4 flex flex-col'>
+                <h2 className='text-lg'>Vehicle Info</h2>
+                <div className='self-center text-slate-500'>
+                    <p title='Vehicle Identification Number'>VIN# {data.vehicle.VIN}</p>
+                    <p className='py-2'>Year: <span className='border py-1 px-4 inline-block'>{data.vehicle.Year}</span></p>
+                    <p className='py-2'>Make: <span className='border py-1 px-4 inline-block'>{data.vehicle.Make}</span></p>
+                    <p className='py-2'>Model: <span className='border py-1 px-4 inline-block'>{data.vehicle.Model}</span></p>
+                    <p className='py-2'>Engine Size: <span className='border py-1 px-4 inline-block'>{data.vehicle.EngineSize}</span></p>
+                </div>
+                <p title='pay type' className='whitespace-pre-wrap'>Pay Type: {repairOrder.isWarranty ? "Warranty":"Customer Pay"}</p>
+            </section>
+            ||  
+            <section className='border-b text-center p-4'>
+                <p title='vehicle vin number'>VIN# {repairOrder.vin}</p>
+                <p className='text-red-500'>{data}! Please try recreating the repair order and check that the vin is correct!</p>
+                <p title='pay type' className='whitespace-pre-wrap'>Pay Type: {repairOrder.isWarranty ? "Warranty":"Customer Pay"}</p>
+            </section>
+        }
+
+        <section className='w-full border-b p-4'>
+            <h2 className='text-lg '>Technician Notes</h2>
+            <p title='technician notes' className='whitespace-pre-line w-full my-6'>{repairOrder.notes}</p>
         </section>
-        <section className='border-b text-center py-4 mb-10'>
-            <p title='vehicle vin number'>VIN # {repairOrder.vin}</p>
-            <p title='pay type' className='whitespace-pre-wrap'>Pay Type: {repairOrder.isWarranty ? "Warranty":"Customer Pay"}</p>
-        </section>
-        <section className='w-full border-b py-4 mb-10'>
-            <p title='technician notes' className='whitespace-pre-line w-full'>Tech Notes: <br />{repairOrder.notes}</p>
-        </section>
-        <section className='flex w-full justify-between py-4'>
+        <section className='flex w-full justify-between p-4'>
             <Link to={`/user/${userId}`} className='underline hover:text-violet-900'>Go to Dashboard</Link>
             <DeleteButton path={'../repairorder/delete/'}  id={repairOrder._id} text={'Delete'}/>
             <Link to={`../editrepairorder/${repairId}`} className='transition ease-in-out delay-100 hover:text-green-300 font-semibold'>Edit <FontAwesomeIcon icon={faPenToSquare} /></Link>
