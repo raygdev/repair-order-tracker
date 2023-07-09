36ccd9e6c933e46450d112a136a119b979be605b
raygdev <101662915+raygdev@users.noreply.github.com>
2023-07-09 02:18:14 -0400

    Remove state and controls
    remove state from login component and remove
    the form controls and errors
    
    prep for validations being returned

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import loadable from "@loadable/component";
 import { useNavigation, useActionData } from "react-router-dom";
 const Form = loadable(() =>
@@ -6,21 +6,8 @@ const Form = loadable(() =>
 );
 
 export default function Login(){
-  const [input, setInput] = useState({
-    email: "",
-    password: "",
-  });
   const navigation = useNavigation();
   const error = useActionData();
-
-  function handleChange(e) {
-    const { name, value } = e.target;
-    setInput((prevInput) => ({
-      ...prevInput,
-      [name]: value,
-    }));
-  }
-
   return (
     <main className="h-screen bg-gradient-to-tl from-blue-300 to-violet-200 flex flex-col w-full justify-center items-center">
       <div className="w-full px-4 xs:max-w-md min-w-max">
@@ -35,30 +22,20 @@ export default function Login(){
           <label htmlFor="email">Email</label>
           <input
             className={`p-1 rounded ${
-              error && !input.email && "border-2 border-red-400"
             }`}
-            type="email"
+            type="text"
             name="email"
-            value={input.email}
             placeholder="Email"
-            onChange={handleChange}
           />
-          {error && !input.email && (
-            <span className="text-red-500">Email required</span>
           )}
           <label htmlFor="password">Password</label>
           <input
             className={`p-1 rounded ${
-              error && !input.password && "border-2 border-red-400"
             }`}
             type="password"
             name="password"
-            value={input.password}
             placeholder="Password"
-            onChange={handleChange}
           />
-          {error && !input.password && (
-            <span className="text-red-500">Password required</span>
           )}
           <button
             className="text-white min-h-max mt-5 disabled:bg-sky-500 bg-sky-700 self-center px-2 py-1 rounded  hover:bg-sky-900  active:bg-sky-300 active:text-slate-700 focus:outline-none focus:ring-4 focus:ring-sky-300"
