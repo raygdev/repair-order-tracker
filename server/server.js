63ee84004551fc7515d0acbcd602c33cae7ece7a
raygdev <101662915+raygdev@users.noreply.github.com>
2023-10-03 17:09:41 -0400

    Move routes to new file
    moves routes to new file and import initApp
    to initialize.
    
    add chalk to logs for better console
    output

--- server/server.js
+++ server/server.js
@@ -1,38 +1,45 @@
 const express = require('express')
-const helmet = require('helmet')
-const cors = require('cors')
-const path = require('path')
 const mongoose = require('mongoose')
+const chalk = require('chalk')
 require('dotenv').config()
 
-const registerRoute = require('./routes/register.js')
-const loginRoute = require('./routes/login.js')
-const userRoutes = require('./routes/user.js')
-const repairOrderRoutes = require('./routes/repairOrderRoutes')
-const vehicleRoutes = require("./routes/vehicle.js")
-const verify = require("./routes/verify.js")
+const { initApp } = require('./initApp')
 
+const log = console.log
 const port = process.env.PORT
 const uri = process.env.MONGO_URI
-
- mongoose.connect(uri,{},(err) => {
-    if(err) return console.error(err.reason)
-})
-
 const app = express()
-app.use(express.static(path.join(__dirname, 'build',)))
 
-app.use(cors())
-app.use(helmet())
-app.use(express.urlencoded({extended:false}))
-app.use(express.json())
-app.use(verify)
-app.use(registerRoute)
-app.use(loginRoute)
-app.use(userRoutes)
-app.use(repairOrderRoutes)
-app.use(vehicleRoutes)
+initApp(app, express)
 
-app.listen(port, () => {
-    console.log(`App listening on port ${port}`)
+// Quiet mongoose warning
+mongoose.set("strictQuery", true)
+
+// Connect to mongo then listen
+mongoose.connect(uri,{},(err) => {
+    if(err){ 
+    // Log error and exit
+      log(
+        chalk.redBright.bold('Error connecting to DB:\n', chalk.bgRedBright("%s")),
+        err.reason || err
+        )
+        process.exitCode = 1
+        return;
+    }
+    app.listen(port, () => {
+        log(
+            chalk.green(
+                `${chalk.magentaBright("Successfully connected to the DB\n")}App listening on port %s`
+                ),
+            chalk.blueBright(port)
+            )
+            process.exitCode = 0
+            return
+    })
 })
+ process.on("exit", (code) => {
+    log(
+        chalk.bgBlue.bold(`Process exited with code %s`),
+        code
+    )
+ })
