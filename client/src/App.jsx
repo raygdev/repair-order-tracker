1af44e28e6419847a8eb9d59f2f160c8b8440ef8
raygdev <101662915+raygdev@users.noreply.github.com>
2024-02-18 00:50:41 -0500

    Remove react default and import jwt and useNavigate

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
-import React, {Suspense} from 'react' 
+import { Suspense, useEffect, useState } from 'react'
 import './App.css';
 import loadable from '@loadable/component';
+import { jwtDecode } from 'jwt-decode'
+import { useNavigate } from 'react-router';
 const Header = loadable(() => import('./components/Header'))
 const Outlet = loadable(() => import('react-router-dom').then(module => ({default : module.Outlet})))
 
