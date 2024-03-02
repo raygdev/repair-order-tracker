d60938075907a0df0b5caa058760ee602b49a337
raygdev <101662915+raygdev@users.noreply.github.com>
2024-03-01 20:40:43 -0500

    Extend themes

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,33 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line
 const defaultTheme = require('tailwindcss/defaultTheme')
 
+// eslint-disable-next-line
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        btn: {
+          primary: '#110372',
+          secondary: '#0455C0'
+        },
+        form: {
+          header: '#0455C0'
+        },
+        status: {
+          completed: '#5CB85C'
+        }
+      },
+      boxShadow: {
+        repair:{
+          card: '0 4px 16px -2px rgba(0,0,0,0.1)',
+          form: '0 4px 16px -2px rgba(0,0,0,0.1)'
+        }
+      },
+    },
     screens:{
       'xs':'500px',
       ...defaultTheme.screens
