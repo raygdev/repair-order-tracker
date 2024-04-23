fc7ecc67bee7d9f6dd70f37ed92a263171c462c3
raygdev <101662915+raygdev@users.noreply.github.com>
2024-04-22 21:47:05 -0400

    Change form styling for register

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,25 +1,26 @@
 import React from "react";
-import { Form, useActionData } from "react-router-dom";
+import { Form, useActionData, useNavigation } from "react-router-dom";
 
 export default function Register (props) {
+  const navigation = useNavigation()
   const inputs = useActionData()
   return (
-    <main className=" flex w-full flex-col min-h-screen bg-gradient-to-br from-blue-800 via-sky-300">
+    <main className=" flex w-full flex-col min-h-screen text-ro-slate-900">
         <div className="w-full xs:max-w-md m-auto">
           <Form
-            className="p-4 min-h-max flex flex-col bg-[#fffff2] justify-between rounded shadow-xl shadow-slate-500"
+            className="p-4 min-h-max flex flex-col justify-between rounded shadow-form"
             method="post"
             action="../register"
           >
             <div className="mb-4 text-center">
-              <h2 className="text-2xl mb-1 self-center bg-gradient-to-r from-blue-900 to-blue-700 bg-clip-text text-transparent font-extrabold">Please Register!</h2>
+              <h2 className="text-2xl mb-1 self-center font-extrabold">Register</h2>
               <p className="text-sm self-center font-medium">It's <span>Free</span>!</p>
             </div>
             {inputs && !inputs.isValid && <span className="text-red-700">Please check your information!</span>}
             <label className="flex flex-col gap-1 my-1">First Name
               <input
                 id='firstName'
-                className={`p-2 rounded ${(inputs && inputs.firstName.isInvalid)? "border-2 border-red-600": "border border-slate-400"}`}
+                className={`p-2 rounded ${(inputs && inputs.firstName.isInvalid)? "border-2 border-red-600": "border-2 border-ro-slate-300"}`}
                 type="text"
                 name="firstName"
                 placeholder="First Name"
@@ -29,7 +30,7 @@ export default function Register (props) {
             <label className="flex flex-col gap-1 my-1">Last Name
               <input
                 id='lastName'
-                className={`p-2 rounded ${(inputs && inputs.lastName.isInvalid)? "border-2 border-red-600": "border border-slate-400"}`}
+                className={`p-2 rounded ${(inputs && inputs.lastName.isInvalid)? "border-2 border-red-600": "border-2 border-ro-slate-300"}`}
                 type="text"
                 name="lastName"
                 placeholder="Last Name"
@@ -39,7 +40,7 @@ export default function Register (props) {
             <label className="flex flex-col gap-1 my-1">Shop Name (optional)
               <input
                 id='shopName'
-                className={`p-2 rounded ${(inputs && inputs.shopName.isInvalid)? "border-2 border-red-600": "border border-slate-400"}`}
+                className={`p-2 rounded ${(inputs && inputs.shopName.isInvalid)? "border-2 border-red-600": "border-2 border-ro-slate-300"}`}
                 type="text"
                 name="shopName"
                 placeholder="Shop Name"
@@ -49,7 +50,7 @@ export default function Register (props) {
             <label className="flex flex-col gap-1 my-1">Email
               <input
                 id='email'
-                className={`p-2 rounded ${(inputs && inputs.email.isInvalid)? "border-2 border-red-600": "border border-slate-400"}`}
+                className={`p-2 rounded ${(inputs && inputs.email.isInvalid)? "border-2 border-red-600": "border-2 border-ro-slate-300"}`}
                 type="text"
                 name="email"
                 placeholder="Email"
@@ -59,15 +60,18 @@ export default function Register (props) {
             <label className="flex flex-col gap-1 my-1">Password
               <input
                 id='password'
-                className={`p-2 rounded ${(inputs && inputs.password.isInvalid)? "border-2 border-red-600": "border border-slate-400"}`}
+                className={`p-2 rounded ${(inputs && inputs.password.isInvalid)? "border-2 border-red-600": "border-2 border-ro-slate-300"}`}
                 type="password"
                 name="password"
                 placeholder="Password"
               />
               {inputs && inputs.password.isInvalid && <div className="whitespace-pre-wrap text-red-600">{inputs.password.message}</div>}
             </label>
-            <button className="text-white bg-sky-700 self-center px-1 py-1 rounded mt-4">
-              Submit
+            <button
+              className="text-ro-slate-100 hover:bg-ro-link-hover disabled:bg-ro-link-disable bg-ro-link-primary self-center px-6 py-2 rounded mt-4"
+              disabled= {navigation.state !== 'idle'}
+            >
+              {navigation.state === 'idle' ? "Submit": "Submitting"}
             </button>
           </Form>
       </div>
