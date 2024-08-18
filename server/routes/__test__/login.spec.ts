359d30bb4c891b44ff34d3f93f0ae801a93e5b39
raygdev <101662915+raygdev@users.noreply.github.com>
2024-08-18 17:47:37 -0400

    Assert login route

---
+++ server/routes/__test__/login.spec.ts
@@ -0,0 +1,58 @@
+import request from "supertest";
+//@ts-ignore
+const { initApp } = require("../../initApp.js");
+import express from "express";
+import { User } from "../../models/user-model";
+
+const app = express();
+initApp(app, express);
+
+const createUser = async () => {
+  const user = {
+    firstName: "Test",
+    lastName: "Test",
+    email: "test@test.com",
+    password: "password",
+  };
+
+  const newUser = new User(user);
+  await newUser.save();
+  return newUser;
+};
+
+describe("POST /api/login", () => {
+  beforeEach(async () => {
+    await createUser();
+  });
+
+  it("fails login with invalid credentials", async () => {
+    const email = "someUser@gmail.com";
+    const password = "doesNotExist";
+    const response = await request(app)
+      .post("/api/login")
+      .send({
+        email,
+        password,
+      })
+      .expect(404);
+
+    expect(response.body).toHaveProperty("message");
+    expect(response.body.message).toEqual(
+      "username/password combination doesn't exist"
+    );
+  });
+
+  it("works with the right credentials", async () => {
+    const email = "test@test.com";
+    const password = "password";
+
+    const response = await request(app)
+      .post("/api/login")
+      .send({
+        email,
+        password,
+      })
+      .expect(200);
+    expect(response.body).toHaveProperty("token");
+  });
+});
