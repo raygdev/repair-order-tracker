95bc70749a10006993704318f045c105eb19953f
raygdev <101662915+raygdev@users.noreply.github.com>
2024-08-19 14:28:38 -0400

    Export app when initialized

--- server/initApp.ts
+++ server/initApp.ts
@@ -1,11 +1,9 @@
-import exp, { Application } from 'express'
-import { NotFoundError } from './errors/not-found-error'
-type Express = typeof exp
-
+import express, { Request, Response } from 'express'
 import helmet from 'helmet'
 import cors from 'cors'
 import path from 'path'
 
+import { NotFoundError } from './errors/not-found-error'
 
 import { registerRoute } from './routes/register'
 import { loginRoute } from './routes/login'
@@ -16,20 +14,22 @@ import { verify } from "./routes/verify"
 import { errorHandler }  from './controllers/middleware/error-handler'
 const origin = process.env.NODE_ENV === 'production' ? 'https://repair-order-tracker.vercel.app' : 'http://localhost:5173'
 
-export function  initApp(app: Application, express: Express){
-    app.use(express.static(path.join(__dirname, 'build',)))
-    app.use(cors({ origin }))
-    app.use(helmet())
-    app.use(express.urlencoded({extended:false}))
-    app.use(express.json())
-    app.use(verify)
-    app.use(registerRoute)
-    app.use(loginRoute)
-    app.use(userRoutes)
-    app.use(repairOrderRoutes)
-    app.use(vehicleRoute)
-    app.use('*', (req, res) => {
-        throw new NotFoundError()
-    })
-    app.use(errorHandler)
-}
+const app = express()
+
+app.use(express.static(path.join(__dirname, 'build',)))
+app.use(cors({ origin }))
+app.use(helmet())
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+app.use(verify)
+app.use(registerRoute)
+app.use(loginRoute)
+app.use(userRoutes)
+app.use(repairOrderRoutes)
+app.use(vehicleRoute)
+app.use('*', (req: Request, res: Response) => {
+    throw new NotFoundError()
+})
+app.use(errorHandler)
+
+export { app }
