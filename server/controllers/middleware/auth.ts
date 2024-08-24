5a781a87864b02a5a91263672e04edba4cdfb5ed
raygdev <101662915+raygdev@users.noreply.github.com>
2024-08-24 15:23:41 -0400

    Refactor and reformat auth file

--- server/controllers/middleware/auth.ts
+++ server/controllers/middleware/auth.ts
@@ -1,45 +1,38 @@
-import jwt from 'jsonwebtoken'
-import { Request, Response, NextFunction, RequestParamHandler } from 'express'
-import { User } from '../../models/user-model'
-import { config } from 'dotenv';
-import { UserDoc } from '../../models/user-model';
-import { NotAuthorizedError } from '../../errors/not-authorized-error';
-config()
+import jwt from "jsonwebtoken";
+import { Request, Response, NextFunction, RequestParamHandler } from "express";
+import { User } from "../../models/user-model";
+import { config } from "dotenv";
+import { NotAuthorizedError } from "../../errors/not-authorized-error";
+config();
 
+export const isAuthenticated = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  if (
+    !req.headers.authorization ||
+    !req.headers.authorization.startsWith("Bearer")
+  ) {
+    throw new NotAuthorizedError();
+  }
 
+  const token = req.headers.authorization.split(" ")[1];
 
+  if (!token) {
+    throw new NotAuthorizedError();
+  }
 
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!) as {
+      id: string;
+    };
+    const user = await User.findById(decoded.id).select("-password");
+    req.user = user!;
 
-
-
-export const isAuthenticated = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    let token;
-
-  try {  
-    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
-
-        token = req.headers.authorization.split(' ')[1]
-        if(!token) {
-            throw new NotAuthorizedError()
-        }
-        
-        const decoded = jwt.verify(token, process.env.JWT_SECRET!) as { id: string }
-        const user = await  User.findById(decoded.id).select('-password')
-
-        
-        req.user = user!
-        
-        next()
-    } else {
-        throw new Error()
-    }
+    next();
   } catch (e) {
-        console.log('token expired')
-
-        res.status(401).json({message: 'Not Authorized'})
-    }
-    // if(!token) return  res.status(401).json({message:'Not authorized, no token'})
-
-    
-
-}
+    console.log("token expired");
+    throw new NotAuthorizedError();
+  }
+};
