1c63f0b25a0222ed0a510cf45ab3aa7a051d0297
raygdev <101662915+raygdev@users.noreply.github.com>
2024-08-24 17:21:38 -0400

    Experiment with new url

--- client/src/utils/url.js
+++ client/src/utils/url.js
@@ -1,8 +1,10 @@
 let url;
 if(import.meta.env.PROD) {
-    url = 'https://repair-order-tracker-production.up.railway.app'
+    url = 'https://repair-tracker-api.vercel.app'
 } else {
     url = 'http://localhost:8080'
 }
+
+//'https://repair-order-tracker-production.up.railway.app'
 
 export { url }
