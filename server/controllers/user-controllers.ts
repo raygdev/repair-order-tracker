79299db159c882fedc8b28ea077025a6abce3c0b
raygdev <101662915+raygdev@users.noreply.github.com>
2024-10-24 21:26:25 -0400

    Convert getUser to async

--- server/controllers/user-controllers.ts
+++ server/controllers/user-controllers.ts
@@ -4,15 +4,6 @@ import { Request, Response, NextFunction } from "express";
 import { findUserById } from "../models/user-model"
 
 export const getUser = async (req: Request,res: Response, next: NextFunction) => {
-  if(req.params.userId !== req.user!.id) return res.status(401).json({ message: 'Not Authorized' })
-  findUserById(req.params.userId,async (err, user) => {
-
-    if(err) return res.status(404).json({message:"something went wrong"});
-
-    if(!user) return res.status(404).json({message:`Can't seem to find that user`});
-
-    return res.status(200).json({
-        ...user.toJSON()
-    })
-  })
+  const user = await findUserById(req.user!.id)
+  res.json(user)
 }
