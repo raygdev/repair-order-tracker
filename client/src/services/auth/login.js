a75f573ae5fba4693eac402996ca32e205916306
raygdev <101662915+raygdev@users.noreply.github.com>
2024-10-24 23:00:53 -0400

    Remove console logs

--- client/src/services/auth/login.js
+++ client/src/services/auth/login.js
@@ -5,7 +5,6 @@ import { setToken } from "../../utils/token";
 
 export async function login(credentials) {
     const res = await client.post('/api/login', credentials)
-    console.log('[STATUSTEXT]', res.status)
     if(res.status >= 400) {
         console.log('[RES NOT OK]')
         throw res.data
