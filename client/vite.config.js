d3471a14b67700d9104928f0760529ced55d00c1
raygdev <101662915+raygdev@users.noreply.github.com>
2024-10-27 17:02:28 -0400

    Add more resolvers

--- client/vite.config.js
+++ client/vite.config.js
@@ -15,7 +15,8 @@ export default defineConfig({
       "@hooks": path.resolve(__dirname, './src/hooks'),
       "@services": path.resolve(__dirname, './src/services'),
       "@pages": path.resolve(__dirname, './src/pages'),
-      "@utils": path.resolve(__dirname, './src/utils')
+      "@utils": path.resolve(__dirname, './src/utils'),
+      "@assets": path.resolve(__dirname, './src/assets')
     }
   }
 })
