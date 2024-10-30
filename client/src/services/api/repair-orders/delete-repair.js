f430c0ab6f7e4f71fee4d484803c5fa741f4babb
raygdev <101662915+raygdev@users.noreply.github.com>
2024-10-29 20:31:55 -0400

    Use auth service logout for listener

--- client/src/services/api/repair-orders/delete-repair.js
+++ client/src/services/api/repair-orders/delete-repair.js
@@ -1,9 +1,11 @@
 import { client } from "../../../utils/url";
+import { authService } from "../../auth";
 
 export async function deleteRepair(id) {
   const res = await client.delete(`/api/repairorder/${id}`);
 
   if (res.status === 401) {
+    authService.logout()
     return false;
   }
 
