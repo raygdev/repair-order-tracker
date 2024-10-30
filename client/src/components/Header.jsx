6de9b8f1093f1cf00dd538c0027b05c611a15963
raygdev <101662915+raygdev@users.noreply.github.com>
2024-10-29 20:35:51 -0400

    Use useAuth for header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import Logout from "./Logout";
-import { useAuth } from "../hooks/useAuth";
+import { useAuth } from "@hooks/useAuth";
 
 export default function Header(props) {
-  const auth = useAuth()
+   const auth = useAuth()
 
   function activeLink({ isActive }) {
     return isActive ? 'p-4 underline underline-offset-4' : 'p-4'
   }
 
   const links = [
-    !auth && { to: '/', name: 'Home'},
-    !auth && { to: 'login', name: 'Login'},
-    !auth && { to: 'register', name: 'Register'},
-    !auth && { to: 'about', name: 'About'},
-    auth && { to: 'dashboard', name: 'Home'}
+    !auth.isAuth && { to: '/', name: 'Home'},
+    !auth.isAuth && { to: 'login', name: 'Login'},
+    !auth.isAuth && { to: 'register', name: 'Register'},
+    !auth.isAuth && { to: 'about', name: 'About'},
+    auth.isAuth && { to: 'dashboard', name: 'Home'}
   ]
 
   return (
@@ -27,7 +27,7 @@ export default function Header(props) {
               (link,i) => <NavLink className={activeLink} to={link.to} key={i}>{link.name}</NavLink>
             )
         }
-        { auth && <Logout/> }
+        { auth.isAuth && <Logout/> }
       </nav>
     </header>
   );
