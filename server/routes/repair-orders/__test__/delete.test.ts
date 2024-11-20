36470a23b0dc4b092646317e3f6140d24abadd2f
raygdev <101662915+raygdev@users.noreply.github.com>
2024-11-20 07:45:20 -0500

    Add boiler plate tests

---
+++ server/routes/repair-orders/__test__/delete.test.ts
@@ -0,0 +1,6 @@
+describe('DELETE api/repairorders', () => {
+    it.todo('should not delete when user is not authorized')
+    it.todo('should not delete with invalid token')
+    it.todo('should not be authorized to delete another user\'s repair order')
+    it.todo('should successfully delete the repair order that belongs to the user')
+})
