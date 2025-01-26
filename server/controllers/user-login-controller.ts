096732088f751aa3a7d097cc15fad7e8d6348fc7
raygdev <101662915+raygdev@users.noreply.github.com>
2025-01-26 15:25:49 -0500

    Remove callback variation and use async findByEmail

--- server/controllers/user-login-controller.ts
+++ server/controllers/user-login-controller.ts
@@ -1,23 +1,19 @@
 import { Request, Response, NextFunction } from 'express'
-import { findUserByEmail } from "../models/user-model";
+import { findByEmail } from "../models/user-model";
 import  bcrypt from "bcrypt";
 import  { generateToken } from './middleware/token-util'
 
-export const userLoginController = (req: Request, res: Response, next: NextFunction) => {
+export const userLoginController = async (req: Request, res: Response, next: NextFunction) => {
   const { email, password } = req.body;
 
   if(!email || !password) return res.status(404).json({message: 'Both fields  are required'})
 
-  findUserByEmail({ email: email }, (err, user) => {
+  const foundUser = await findByEmail(email)
 
-    if (err) return res.send(err);
+  if (!foundUser) return res.status(404).json({ message: `username/password combination doesn't exist` });
+  if (!bcrypt.compareSync(password, foundUser.password)) return res.status(404).json({ message: `username/password combination doesn't exist` });
 
-    if (!user) return res.status(404).json({ message: `username/password combination doesn't exist` });
+  const user = foundUser.toJSON()
 
-    if (!bcrypt.compareSync(password, user.password)) return res.status(404).json({ message: `username/password combination doesn't exist` });
-
-    delete user._doc.password
-    
-    return res.status(200).json({...user._doc, token: generateToken(user.id)});
-  });
+  return res.status(200).json({...user, password: null, token: generateToken(user.id)});
 };
