2a2e3cecbd728450b1696d2bc64d1357198b9ffa
raygdev <101662915+raygdev@users.noreply.github.com>
2025-02-17 22:21:28 -0500

    Add icon and styles to logout button

--- client/src/components/Logout.jsx
+++ client/src/components/Logout.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Form } from 'react-router-dom'
+import { faSignOut } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function Logout(props) {
   return (
     <Form action='/logout' method='POST'>
-        <button className={`p-4 'underline underline-offset-4' : ''`}>Logout</button>
+        <button className='flex gap-4 items-center text-slate-600'><FontAwesomeIcon aria-hidden={true} icon={faSignOut}/>Log out</button>
     </Form>
   )
 }
