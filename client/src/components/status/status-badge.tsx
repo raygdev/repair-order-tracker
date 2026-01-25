cc32b4ef50d4c0ce9eda54ea524026cc0b97c924
raygdev <raymond.greeley.dev@gmail.com>
2026-01-25 18:49:10 -0500

    fix: make sure badge renders from tailwind upgrade

--- client/src/components/status/status-badge.tsx
+++ client/src/components/status/status-badge.tsx
@@ -1,5 +1,5 @@
 import { Badge } from "@/components/ui/badge";
-import { Timer, Clock8, CircleCheck, CircleX, MessageSquareQuote  } from "lucide-react";
+import { Timer, Clock8, CircleCheck, CircleX, MessageSquareQuote, CircleDot  } from "lucide-react";
 import { cva } from "class-variance-authority";
 
 export interface StatusBadgeProps {
@@ -12,28 +12,23 @@ export const variantText =  {
     "waiting-on-customer": 'Waiting on customer',
     'waiting-on-parts': 'Waiting on parts',
     completed: 'Completed',
-    quoted: 'Quoted'
+    quoted: 'Quoted',
+    "in-progress": 'In Progress'
 }
 
 const colorVariant: Record<keyof typeof variantText, string> = {
-    approved: 'bg-green-50 text-blue-700 dark:bg-blue-950 dark:text-blue-50',
+    approved: 'bg-green-50 text-green-700 dark:bg-green-950 dark:text-green-50',
     declined: 'bg-red-50 text-red-700 dark:bg-red-950 dark:text-red-50',
     "waiting-on-customer": 'bg-sky-50 text-sky-700 dark:bg-sky-950 dark:text-sky-50',
     "waiting-on-parts": 'bg-sky-50 text-sky-700 dark:bg-sky-950 dark:text-sky-50',
-    completed: 'bg-green-50 text-blue-700 dark:bg-blue-950 dark:text-blue-50',
-    quoted: 'bg-neutral-50 text-neutral-700 dark:bg-neutral-950 dark:text-neutral-300'
+    completed: 'bg-green-50 text-green-700 dark:bg-green-950 dark:text-green-50',
+    quoted: 'bg-neutral-50 text-neutral-700 dark:bg-neutral-950 dark:text-neutral-300',
+    "in-progress": 'bg-orange-50 text-orange-700 dark:bg-orange-950 dark:text-orange-300'
 }
 
-const statusBadgeColors = cva("flex gap-2", {
+const statusBadgeColors = cva("gap-2", {
     variants: {
-        variant: {
-           approved: 'bg-green-50 text-blue-700 dark:bg-blue-950 dark:text-blue-50',
-    declined: 'bg-red-50 text-red-700 dark:bg-red-950 dark:text-red-50',
-    "waiting-on-customer": 'bg-sky-50 text-sky-700 dark:bg-sky-950 dark:text-sky-50',
-    "waiting-on-parts": 'bg-sky-50 text-sky-700 dark:bg-sky-950 dark:text-sky-50',
-    completed: 'bg-green-50 text-blue-700 dark:bg-blue-950 dark:text-blue-50',
-    quoted: 'bg-neutral-50 text-neutral-700 dark:bg-neutral-950 dark:text-neutral-300' 
-        }
+        variant: colorVariant
     }
 })
 
@@ -43,12 +38,13 @@ const iconVariant: Record<keyof typeof variantText, React.ReactNode> = {
     "waiting-on-customer": <Clock8 size={16}/>,
     'waiting-on-parts': <Timer size={16}/>,
     completed: <CircleCheck  size={16}/>,
-    quoted: <MessageSquareQuote size={16}/>
+    quoted: <MessageSquareQuote size={16}/>,
+    "in-progress": <CircleDot size={16} />
 }
 
 export function StatusBadge({ variant }: StatusBadgeProps) {
     return (
-        <Badge variant={'destructive'} className='bg-green-50'>
+        <Badge variant={'destructive'} className={statusBadgeColors({ variant })}>
             {iconVariant[variant]} {variantText[variant]}
         </Badge>
     )
