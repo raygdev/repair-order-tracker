6ae92a47a36ec32ad19a4a286e5a960d62506d77
raygdev <raymond.greeley.dev@gmail.com>
2026-02-01 12:35:26 -0500

    refactor: user withRouter helper in story

--- client/src/features/repair-orders/src/components/job/job.stories.tsx
+++ client/src/features/repair-orders/src/components/job/job.stories.tsx
@@ -1,16 +1,54 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
-import { MemoryRouter } from "react-router";
-import { Job } from "./job";
+import { withRouter } from '.storybook/withRouter'
+import { Job, type JobProps } from "./job";
+import { redirect } from "react-router";
+
+const job: JobProps = {
+    job: {
+         id: '123',
+        description: "Remove and replace purge valve",
+        labor: 2.4,
+        status: "approved",
+        parts: [{ price: 12.45, name: "Purge Valve", id: '1' }, { price: 50.47, name: "Intake Gaskets", id: '2' }]
+    }
+}
+
+ function Wrapper() {
+    return <Job {...job} />
+}
+
+
 
 const meta: Meta<typeof Job> = {
     title: 'Features/RepairOrders/Components/Job',
     component: Job,
     decorators: [
-        (Story) => (
-            <MemoryRouter>
-                <Story />
-            </MemoryRouter>
-        )
+        withRouter({
+            routes: [{
+                path: 'dashboard/repairorders/:repairId',
+                element: <Wrapper />,
+                children: [
+                    {
+                        path: 'job/:jobId',
+                        action: async ({ request }) => {
+                            console.log(await request.json())
+                            redirect('/dashboard/repairorders/123')
+                            return null
+                        }
+                    },
+                    {
+                        path: 'part/:partId',
+                        action: async({ request }) => {
+                            console.log(await request.json())
+                            redirect('/dashboard/repairorders/123')
+                            return null
+                        }
+                    }
+                ]
+            }],
+            initialEntries: ['/dashboard/repairorders/123']
+        })
+
     ]
 }
 
@@ -20,12 +58,6 @@ type Story = StoryObj<typeof Job>
 
 export const Playground: Story = {
     args: {
-        job: {
-            id: '123',
-            description: "Remove and replace purge valve",
-            labor: 2.4,
-            status: "approved",
-            parts: [{ price: 12.45, name: "Purge Valve", id: '1' }, { price: 50.47, name: "Intake Gaskets", id: '2' }]
-        }
+        job: job.job
     }
 }
