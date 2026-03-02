f073cc545528b19033f1f5c38c63591c0378b0e5
raygdev <raymond.greeley.dev@gmail.com>
2026-03-01 20:49:00 -0500

    refactor(repair-orders): move services -> adaptors and remove actions

--- client/src/features/repair-orders/src/lib/adapters/repiar-order.service.ts
+++ client/src/features/repair-orders/src/lib/adapters/repiar-order.service.ts
@@ -1,29 +1,25 @@
 import { client } from "@features/utils/url";
-
 import { authService } from "@services/auth";
-import type { RepairOrder } from "../../models/repair-order.models";
 
-interface PartialRepair extends Partial<Omit<RepairOrder, "vehicle" | "job">> {}
-export interface Repair extends PartialRepair {}
+import {
+  type RepairOrder,
+  type RepairCreate,
+  type Repair,
+  RepairOrderRepositoryPort
+} from '../domain'
 
-export interface RepairCreate extends Omit<
-  RepairOrder,
-  "id" | "_id" | "vehicle" | "job" | "status"
-> {
-  status?: RepairOrder["status"];
-}
 
-class RepairOrderService {
+class RepairOrderService extends RepairOrderRepositoryPort{
   constructor(
     private api: typeof client,
     private auth: typeof authService,
-  ) {}
+    
+  ) {
+    super()
+  }
 
   async updateRepair(repair: Repair) {
-    const res = await this.api.put<RepairOrder, any, Repair>(
-      `/api/repairorder/${repair.id}`,
-      repair,
-    );
+    const res = await this.api.put<RepairOrder, any, Repair>(`${this.baseUrl}/${repair.id}`, repair);
 
     if (res.status === 401) {
       this.auth.logout();
@@ -38,7 +34,7 @@ class RepairOrderService {
   }
 
   async createRepair(repair: RepairCreate) {
-    const res = await this.api.post<RepairOrder>("/api/repairorder", repair);
+    const res = await this.api.post<RepairOrder>(this.baseUrl, repair);
 
     if (res.status === 401) {
       this.auth.logout();
@@ -52,8 +48,8 @@ class RepairOrderService {
     return true;
   }
 
-  async getRepairOrders() {
-    const res = await client.get("/api/repairorders");
+  async getAllRepairOrders() {
+    const res = await this.api.get(this.baseUrl);
 
     if (res.status === 401) {
       authService.logout();
@@ -70,7 +66,7 @@ class RepairOrderService {
   }
 
   async deleteRepair(id: string) {
-    const res = await client.delete(`/api/repairorder/${id}`);
+    const res = await this.api.delete(`${this.baseUrl}/${id}`);
 
     if (res.status === 401) {
       authService.logout();
