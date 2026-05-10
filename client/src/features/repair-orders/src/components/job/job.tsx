e3cc1af0cfa6c77d6ea5effe5897c183f319abf7
raygdev <raymond.greeley.dev@gmail.com>
2026-05-10 18:48:27 -0400

    refactor: add delete confirmation and reformat

--- client/src/features/repair-orders/src/components/job/job.tsx
+++ client/src/features/repair-orders/src/components/job/job.tsx
@@ -1,95 +1,99 @@
+import { useState } from "react";
+import { Ellipsis, Trash, SquarePen } from "lucide-react";
+import { DropdownMenuItem } from "@radix-ui/react-dropdown-menu";
 import {
-    Table,
-    TableBody,
-    TableCaption,
-    TableCell,
-    TableFooter,
-    TableHead,
-    TableRow,
-    TableHeader
-} from '@components/ui/table'
+  Table,
+  TableBody,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableRow,
+  TableHeader,
+} from "@components/ui/table";
 import {
-DropdownMenu,
-DropdownMenuContent,
-DropdownMenuTrigger
-} from '@/components/ui/dropdown-menu'
-import { Ellipsis, Trash, SquarePen } from 'lucide-react'
-import { Button } from '@components/ui/button';
-import { StatusBadge } from '@components/status/status-badge';
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@components/ui/button";
+import { StatusBadge } from "@components/status/status-badge";
 
 import type { Job, Part } from "../../lib/domain/models/job.model";
-import { DropdownMenuItem } from '@radix-ui/react-dropdown-menu';
-import { useSubmit } from 'react-router-dom';
-
+import { DeleteConfirmation } from "@features/shared/src/components/delete-confirmation/delete-confirmation";
 
 export interface JobProps {
-    job: Job
+  job: Job;
 }
 
-/** 
+/**
  * @todo add functionality to job button
  */
 
 export function Job({ job }: JobProps) {
-
-
-    const partsTotal = job.parts?.reduce((a,b) => a + b.price || 0, 0).toFixed(2) || 0
-    const totalLabor = (job.labor * 150).toFixed(2)
-    const totalJob = (+totalLabor + +partsTotal).toFixed(2)
-    const rowBorderStyles = 'border-gray-300 hover:bg-gray-50'
-    return (
-        <section aria-labelledby='job-description'>
-            <div className='mb-4 py-5 px-1 rounded border-gray-200 border-1 flex justify-between items-center'>
-                <h2 className='font-bold' id='job-description'>{job.description}</h2>
-                <div className='flex gap-4'>
-                    <StatusBadge variant={job.status} />
-                    <Button variant='ghost' size='icon' aria-label={`More actions for job: ${job.description}`} >
-                        <Ellipsis />
-                    </Button>
-                </div>
-            </div>
-            <div>
-                <div className='py-2 pl-2 flex items-center gap-4'>
-                    <span className='text-sm font-bold'>Labor</span>
-                    <span>{job.labor} hours</span>
-                </div>
-                <Table>
-                    <TableHeader>
-                        <TableRow className={rowBorderStyles}>
-                            <TableHead>Part</TableHead>
-                            <TableHead>Price</TableHead>
-                            <TableHead className='text-right'>Actions</TableHead>
-                        </TableRow>
-                    </TableHeader>
-                    <TableBody>
-                        { job.parts?.map(part => (
-                            <TableRow className={rowBorderStyles} key={part.id}>
-                                <TableCell>{part.name}</TableCell>
-                                <TableCell>${part.price}</TableCell>
-                                <TableCell className='text-right'><Actions part={part} /></TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                    <TableFooter>
-                        <TableRow className={rowBorderStyles}>
-                            <TableCell>Total Parts</TableCell>
-                            <TableCell colSpan={2}>
-                            ${partsTotal}
-                            </TableCell>
-                        </TableRow>
-                        <TableRow className={rowBorderStyles}>
-                            <TableCell className='font-bold'>Total Job</TableCell>
-                            <TableCell colSpan={2}>${totalJob}</TableCell>
-                        </TableRow>
-                    </TableFooter>
-                </Table>
-            </div>
-        </section>
-    )
+  const partsTotal =
+    job.parts?.reduce((a, b) => a + b.price || 0, 0).toFixed(2) || 0;
+  const totalLabor = (job.labor * 150).toFixed(2);
+  const totalJob = (+totalLabor + +partsTotal).toFixed(2);
+  const rowBorderStyles = "border-gray-300 hover:bg-gray-50";
+  return (
+    <section aria-labelledby="job-description">
+      <div className="mb-4 py-5 px-1 rounded border-gray-200 border-1 flex justify-between items-center">
+        <h2 className="font-bold" id="job-description">
+          {job.description}
+        </h2>
+        <div className="flex gap-4">
+          <StatusBadge variant={job.status} />
+          <Button
+            variant="ghost"
+            size="icon"
+            aria-label={`More actions for job: ${job.description}`}
+          >
+            <Ellipsis />
+          </Button>
+        </div>
+      </div>
+      <div>
+        <div className="py-2 pl-2 flex items-center gap-4">
+          <span className="text-sm font-bold">Labor</span>
+          <span>{job.labor} hours</span>
+        </div>
+        <Table>
+          <TableHeader>
+            <TableRow className={rowBorderStyles}>
+              <TableHead>Part</TableHead>
+              <TableHead>Price</TableHead>
+              <TableHead className="text-right">Actions</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {job.parts?.map((part) => (
+              <TableRow className={rowBorderStyles} key={part.id}>
+                <TableCell>{part.name}</TableCell>
+                <TableCell>${part.price}</TableCell>
+                <TableCell className="text-right">
+                  <Actions part={part} />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+          <TableFooter>
+            <TableRow className={rowBorderStyles}>
+              <TableCell>Total Parts</TableCell>
+              <TableCell colSpan={2}>${partsTotal}</TableCell>
+            </TableRow>
+            <TableRow className={rowBorderStyles}>
+              <TableCell className="font-bold">Total Job</TableCell>
+              <TableCell colSpan={2}>${totalJob}</TableCell>
+            </TableRow>
+          </TableFooter>
+        </Table>
+      </div>
+    </section>
+  );
 }
 
 interface ActionProps {
-    part: Part
+  part: Part;
 }
 
 /**
@@ -97,29 +101,43 @@ interface ActionProps {
  */
 
 function Actions({ part }: ActionProps) {
+  const [open, setOpen] = useState(false);
 
-    const submit = useSubmit()
-    const submitType = { method: 'delete', encType: 'applicaiton/json' }
-
-    const baseItemStyles = "flex items-center gap-2 cursor-pointer hover:bg-gray-50"
-    return (
-        <DropdownMenu modal={false}>
-            <DropdownMenuTrigger asChild>
-                <Button className='cursor-pointer' variant='ghost' size='icon' aria-label={`More actions for ${part.name}`}>
-                    <Ellipsis />
-                </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align='end' className='bg-white border-slate-50'>
-                    <DropdownMenuItem className={baseItemStyles}>
-                        <SquarePen size={16} /> Edit <span className='sr-only'>{part.name}</span>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem
-                      onClick={() => submit({ id: part.id }, { method: 'delete', action: `part/${part.id}`, encType: 'application/json'})}
-                      className={`text-red-600 ${baseItemStyles}`}
-                    >
-                        <Trash size={16}/> Delete <span className='sr-only'>{part.name}</span>
-                    </DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
-    )
+  const baseItemStyles =
+    "flex items-center gap-2 cursor-pointer hover:bg-gray-50";
+  return (
+    <>
+      <DropdownMenu modal={false}>
+        <DropdownMenuTrigger asChild>
+          <Button
+            className="cursor-pointer"
+            variant="ghost"
+            size="icon"
+            aria-label={`More actions for ${part.name}`}
+          >
+            <Ellipsis />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end" className="bg-white border-slate-50">
+          <DropdownMenuItem className={baseItemStyles}>
+            <SquarePen size={16} /> Edit{" "}
+            <span className="sr-only">{part.name}</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            onClick={() => setOpen(true)}
+            className={`text-red-600 ${baseItemStyles}`}
+          >
+            <Trash size={16} /> Delete{" "}
+            <span className="sr-only">{part.name}</span>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <DeleteConfirmation
+        open={open}
+        setOpen={setOpen}
+        id={part.id}
+        name={part.name}
+      />
+    </>
+  );
 }
