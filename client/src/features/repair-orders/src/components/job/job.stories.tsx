1d5a435ee7b348c634d459ca3af6644f63a74a8d
raygdev <raymond.greeley.dev@gmail.com>
2026-05-10 18:49:01 -0400

    chore: add sleep function and reformat

--- client/src/features/repair-orders/src/components/job/job.stories.tsx
+++ client/src/features/repair-orders/src/components/job/job.stories.tsx
@@ -1,63 +1,66 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
-import { withRouter } from '.storybook/withRouter'
+import { withRouter } from ".storybook/withRouter";
 import { Job, type JobProps } from "./job";
 import { redirect } from "react-router";
+import { sleep } from "@features/utils/sleep";
 
 const job: JobProps = {
-    job: {
-         id: '123',
-        description: "Remove and replace purge valve",
-        labor: 2.4,
-        status: "approved",
-        parts: [{ price: 12.45, name: "Purge Valve", id: '1' }, { price: 50.47, name: "Intake Gaskets", id: '2' }]
-    }
+  job: {
+    id: "123",
+    description: "Remove and replace purge valve",
+    labor: 2.4,
+    status: "approved",
+    parts: [
+      { price: 12.45, name: "Purge Valve", id: "1" },
+      { price: 50.47, name: "Intake Gaskets", id: "2" },
+    ],
+  },
+};
+
+function Wrapper() {
+  return <Job {...job} />;
 }
-
- function Wrapper() {
-    return <Job {...job} />
-}
-
-
 
 const meta: Meta<typeof Job> = {
-    title: 'Features/RepairOrders/Components/Job',
-    component: Job,
-    decorators: [
-        withRouter({
-            routes: [{
-                path: 'dashboard/repairorders/:repairId',
-                element: <Wrapper />,
-                children: [
-                    {
-                        path: 'job/:jobId',
-                        action: async ({ request }) => {
-                            console.log(await request.json())
-                            redirect('/dashboard/repairorders/123')
-                            return null
-                        }
-                    },
-                    {
-                        path: 'part/:partId',
-                        action: async({ request }) => {
-                            console.log(await request.json())
-                            redirect('/dashboard/repairorders/123')
-                            return null
-                        }
-                    }
-                ]
-            }],
-            initialEntries: ['/dashboard/repairorders/123']
-        })
-
-    ]
-}
+  title: "Features/RepairOrders/Components/Job",
+  component: Job,
+  decorators: [
+    withRouter({
+      routes: [
+        {
+          path: "dashboard/repairorders/:repairId",
+          element: <Wrapper />,
+          children: [
+            {
+              path: "job/:jobId",
+              action: async ({ request }) => {
+                console.log(await request.json());
+                redirect("/dashboard/repairorders/123");
+                return null;
+              },
+            },
+            {
+              path: "part/:partId",
+              action: async ({ request }) => {
+                console.log(await request.json());
+                await sleep();
+                return { success: true };
+              },
+            },
+          ],
+        },
+      ],
+      initialEntries: ["/dashboard/repairorders/123"],
+    }),
+  ],
+};
 
 export default meta;
 
-type Story = StoryObj<typeof Job>
+type Story = StoryObj<typeof Job>;
 
 export const Playground: Story = {
-    args: {
-        job: job.job
-    }
-}
+  args: {
+    job: job.job,
+  },
+};
